b8cadc827343014d14e7e8a921c4d0af1a83ee72
GLdL <guillermo.jose.loustau.de.linares@cern.ch>
2019-11-29 10:19:42 +0100

    fix he.tcl

--- Top/he/he.tcl
+++ Top/he/he.tcl
@@ -4,10 +4,14 @@ set use_questa_simulator 0
 
 ## FPGA and Vivado strategies and flows
 set FPGA xcvu5p-flvb2104-2-i
-set SYNTH_STRATEGY "Flow_AreaOptimized_High" 
-set SYNTH_FLOW "Vivado Synthesis 2017"
+set SYNTH_STRATEGY "Flow_PerfOptimized_high" 
+set SYNTH_FLOW "Vivado Synthesis 2019"
 set IMPL_STRATEGY "Performance_ExplorePostRoutePhysOpt"
-set IMPL_FLOW "Vivado Implementation 2017"
+set IMPL_FLOW "Vivado Implementation 2019"
+
+# set_property flow {Vivado Synthesis 2019} [get_runs synth_1]
+# set_property write_incremental_synth_checkpoint true [get_runs synth_1]
+# set_property strategy Flow_PerfOptimized_high [get_runs synth_1]
 
 ############################################################
 set DESIGN    "[file rootname [file tail [info script]]]"
