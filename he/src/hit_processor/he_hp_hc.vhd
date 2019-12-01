fb96698180fa63d6ad65a314172ec34f15939455
GLdL <guillermo.jose.loustau.de.linares@cern.ch>
2019-12-01 20:59:22 +0100

    rename core -> processor

---
+++ he/src/hit_processor/he_hp_hc.vhd
@@ -0,0 +1,38 @@
+--------------------------------------------------------------------------------
+--  UMass , Physics Department
+--  Guillermo Loustau de Linares
+--  gloustau@cern.ch
+--  
+--  Project: ATLAS L0MDT Trigger 
+--  Module:
+--  Description:
+--
+--------------------------------------------------------------------------------
+--  Revisions:
+--   29/11/2019  0.1  File created
+--------------------------------------------------------------------------------
+
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+library he_lib;
+use he_lib.he_pkg.all;
+
+entity he_c_hc is
+    port (
+        clk_360             : in std_logic;
+        Reset_b             : in std_logic;
+        --
+
+    );
+end entity he_c_hc;
+
+architecture beh of he_c_hc is
+
+
+begin
+
+
+end beh;
