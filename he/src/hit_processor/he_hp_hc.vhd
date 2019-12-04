0cf49a758d5b896e99b67905d4c87fe5c4be7f96
GLdL <guillermo.jose.loustau.de.linares@cern.ch>
2019-12-04 15:58:04 +0100

    main structure done, RTL ok

--- he/src/hit_processor/he_hp_hc.vhd
+++ he/src/hit_processor/he_hp_hc.vhd
@@ -20,16 +20,25 @@ use ieee.numeric_std.all;
 library he_lib;
 use he_lib.he_pkg.all;
 
-entity he_c_hc is
+entity he_hp_hc is
     port (
         clk_360             : in std_logic;
         Reset_b             : in std_logic;
         --
+        tdchits_r                : in tar2hec_data_rt;
+        tdchits_valid            : in std_logic;
+        muon_candidate_info_r         : in muon_candidate_info_rt;
+        muon_candidate_info_valid   : in std_logic;
+        --
+        cal_time        : out integer;
+        cal_time_valid  : out std_logic;
+        sf_data         : out segment_finder_legendre_rt;
+        sf_data_valid   : out std_logic
 
     );
-end entity he_c_hc;
+end entity he_hp_hc;
 
-architecture beh of he_c_hc is
+architecture beh of he_hp_hc is
 
 
 begin
--- he/src/hit_processor/he_hp_trlut.vhd
+++ he/src/hit_processor/he_hp_trlut.vhd
@@ -20,16 +20,24 @@ use ieee.numeric_std.all;
 library he_lib;
 use he_lib.he_pkg.all;
 
-entity he_c_crlut is
+entity he_hp_crlut is
     port (
         clk_360             : in std_logic;
         Reset_b             : in std_logic;
         --
+        hit_layer                   : in integer;
+        hit_layer_valid             : in std_logic;
+        tube_ranges_config          : in tube_range_config_rt;
+        tube_ranges_config_valid    : in std_logic;
+        --
+        tube_range          : out tube_range_cand_rt;
+        out_data_valid      : out std_logic
+
 
     );
-end entity he_c_crlut;
+end entity he_hp_crlut;
 
-architecture beh of he_c_crlut is
+architecture beh of he_hp_crlut is
 
 
 begin
