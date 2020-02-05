b3153201dc0c23f27872b9c06ad3a19b1d47d458
GLdL <guillermo.jose.loustau.de.linares@cern.ch>
2020-02-05 19:19:42 +0100

    problems with shared/con/

--- Top/csf/csf.tcl
+++ Top/csf/csf.tcl
@@ -29,6 +29,7 @@ set PROPERTIES [dict create \
 			       ]\
 		   ]
 ############################################################
+###########################################################
 ############################################################
 set DESIGN    "[file rootname [file tail [info script]]]"
 set path_repo "[file normalize [file dirname [info script]]]/../../"
