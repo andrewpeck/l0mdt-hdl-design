13310722f491ce75c38247ee0609c18c16050881
GLdL <guillermo.jose.loustau.de.linares@cern.ch>
2020-02-15 10:02:16 +0100

    hp block in progress, trLUT vhdlROM created

---
+++ hp/src/ROMs/hp_trlut_rom_s3.vhd
@@ -0,0 +1,55 @@
+--------------------------------------------------------------------------------
+--  UMass , Physics Department
+--  Guillermo Loustau de Linares
+--  gloustau@cern.ch
+--  
+--  Project: ATLAS L0MDT Trigger 
+--  Module: Tube range LUT for sector 3
+--  Description:
+--
+--------------------------------------------------------------------------------
+--  Revisions:
+--    05/02/2020  0.1  File created
+--------------------------------------------------------------------------------
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+use ieee.math_real.all;
+
+package hp_trLUT_s3_pkg is
+
+    type trLUT_limits_t is array (0 to 1) of integer;
+    type trLUT_layer_t is array (0 to 7) of trLUT_limits_t;
+    type trLUT_station_t is array (0 to 5) of trLUT_layer_t;
+    type trLut_sector_t is array ( 0 to 3) of trLUT_station_t;
+
+    constant trLUT_s3i_rom_mem : trLUT_station_t := (
+        ((1,1),(1,2),(0,3),(0,4),(0,5),(0,6),(0,7),(0,8)),
+        ((2,1),(2,2),(0,3),(0,4),(0,5),(0,6),(0,7),(0,8)),
+        ((3,1),(3,2),(0,3),(0,4),(0,5),(0,6),(0,7),(0,8)),
+        ((4,1),(4,2),(0,3),(0,4),(0,5),(0,6),(0,7),(0,8)),
+        ((5,1),(5,2),(0,3),(0,4),(0,5),(0,6),(0,7),(0,8)),
+        ((6,1),(6,2),(0,3),(0,4),(0,5),(0,6),(0,7),(0,8))
+    );
+
+    constant trLUT_s3m_rom_mem : trLUT_station_t := (
+        (others => (others => (0,0)))
+    );
+    
+
+    constant trLUT_s3o_rom_mem : trLUT_station_t := (
+        (others => (others => (0,0)))
+    );
+
+    constant trLUT_s3e_rom_mem : trLUT_station_t := (
+        (others => (others => (0,0)))
+    );
+
+    constant trLUT_s3_mem : trLut_sector_t :=(
+        trLUT_s3i_rom_mem , trLUT_s3m_rom_mem , trLUT_s3o_rom_mem , trLUT_s3e_rom_mem
+    );
+
+
+
+end package hp_trLUT_s3_pkg;
--- hp/src/hp_hit_matching.vhd
+++ hp/src/hp_hit_matching.vhd
@@ -48,6 +48,10 @@ end entity hp_matching;
 architecture beh of hp_matching is
 
     component hp_m_trLUT is
+        generic(
+            tube_min : integer;
+            tube_max : integer
+        );
         port (
             clk                 : in std_logic;
             -- Control
@@ -68,9 +72,15 @@ architecture beh of hp_matching is
     signal tube_high_limit, tube_low_limit : unsigned(mdt_tube_bits - 1 downto 0);
     signal trLUT_valid : std_logic;
 
+    signal space_valid,time_valid : std_logic;
+
 begin
 
     HP_HMTRLUT : hp_m_trLUT
+    generic map(
+        tube_min => 3,
+        tube_max => 5
+    )
     port map(
         clk                 => clk,
         -- Control
@@ -86,6 +96,28 @@ begin
         o_tube_low_limit    => tube_low_limit,
         o_data_valid        => trLUT_valid
     );
+
+
+    o_hit_valid <= space_valid and time_valid;
+
+    validation_proc : process(clk,Reset_b)
+
+    begin
+        if not Reset_b then
+            space_valid <= '0';
+            time_valid <= '0';
+        elsif rising_edge(clk) then
+            -- space
+            if i_tdc_tube <= tube_high_limit and i_tdc_tube >= tube_low_limit then
+                space_valid <= '1';
+            end if;
+            -- time
+            
+            --valid
+            o_data_valid <= trLUT_valid;
+        end if;
+
+    end process;
     
     
 end beh;
@@ -106,6 +138,7 @@ end beh;
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
+use ieee.math_real.all;
 
 library l0mdt_lib;
 use l0mdt_lib.cfg_pkg.all;
@@ -113,8 +146,14 @@ use l0mdt_lib.common_pkg.all;
 
 library hp_lib;
 use hp_lib.hp_pkg.all;
+use hp_lib.hp_trLUT_s3_pkg.all;
 
 entity hp_m_trLUT is
+    generic(
+
+        tube_min : integer;
+        tube_max : integer
+    );
     port (
         clk                 : in std_logic;
         -- Control
@@ -125,7 +164,7 @@ entity hp_m_trLUT is
         -- MDT hit
         i_tdc_layer         : in unsigned(mdt_layer_bits -1 downto 0);
         i_tdc_valid         : in std_logic;
-        -- to Segment finder
+        -- to matching
         o_tube_high_limit   : out unsigned(mdt_tube_bits - 1 downto 0);
         o_tube_low_limit    : out unsigned(mdt_tube_bits - 1 downto 0);
         o_data_valid        : out std_logic
@@ -134,7 +173,37 @@ end entity hp_m_trLUT;
 
 architecture beh of hp_m_trLUT is
 
+    type LUT_mem_t is array (0 to (tube_max - tube_min) - 1) of trLUT_layer_t;
+
+    signal LUT_mem : LUT_mem_t;
+    
+    signal lut_index : integer;
+
 begin
+
+    cpy_mem : for x in 0 to (tube_max - tube_min - 1)  generate
+        LUT_mem(x) <= trLUT_s3_mem(0)(tube_min + x);
+    end generate;
+
+    lut_index <= abs(to_integer(i_SLc_z_pos))  - (tube_max - tube_min)/2;
+
+    LUT : process(clk,Reset_b)
+
+    begin
+        if not Reset_b then
+            o_tube_high_limit <= (others => '0');
+            o_tube_low_limit <= (others => '0');
+            o_data_valid <= '0';
+        elsif rising_edge(clk) then
+            if lut_index < (tube_max - tube_min - 1) then
+                o_tube_high_limit <= to_unsigned(LUT_mem(lut_index)(to_integer(i_tdc_layer))(1),mdt_tube_bits);
+                o_tube_low_limit <= to_unsigned(LUT_mem(lut_index)(to_integer(i_tdc_layer))(0),mdt_tube_bits);
+                o_data_valid <= i_tdc_valid;
+            else
+
+            end if;
+        end if ;
+    end process;
     
     
 end beh;
