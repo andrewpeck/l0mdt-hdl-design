73b5f8683e5ccd95f1514dfee69dba06e0a4b80d
Andrew Peck <andrew.peck@cern.ch>
2020-04-22 13:12:40 -0400

    Add GTY support to SL MGT

--- hal/src/mgt/mgt_sl_wrapper.vhd
+++ hal/src/mgt/mgt_sl_wrapper.vhd
@@ -14,7 +14,7 @@ entity mgt_sl_wrapper is
 
   generic(
     index   : integer    := 0;
-    gt_type : gt_types_t := GTH
+    gt_type : gt_types_t := GTY
     );
   port(
     --=============--
@@ -185,28 +185,106 @@ begin
 
   end generate;
 
---   gty_gen : if (gt_type = GTY) generate
---
---     attribute X_LOC            : integer;
---     attribute Y_LOC            : integer;
---     attribute X_LOC of MGT_GEN : label is c_MGT_MAP(index).x_loc;
---     attribute Y_LOC of MGT_GEN : label is c_MGT_MAP(index).y_loc;
---
---     attribute NUM_MGTS            : integer; -- need it somewhere more handy actually...
---     attribute NUM_MGTS of MGT_GEN : label is c_NUM_MGTS;  -- make a copy of this handy for tcl
---
---   begin
---
---
---   end generate;
---
--- --    gty_gen : if (gt_type = GTY) generate
--- --      assert false report "GENERATING GTY LINK not yet supported" severity error;
--- --    end generate gty_gen;
--- --
--- --    nil_gen : if (gt_type = GT_NIL) generate
--- --      assert false report "GENERATING NIL link NEVER supported" severity error;
--- --    end generate nil_gen;
--- --  end generate;
+  -- TODO: these different generators are EXACTLY the same except the string "gth" and "gty" in 4 ports...
+  -- is there any clever way to automate this?
+  gty_gen : if (gt_type = GTY) generate
+
+    attribute X_LOC            : integer;
+    attribute Y_LOC            : integer;
+    attribute X_LOC of MGT_GEN : label is c_MGT_MAP(index).x_loc;
+    attribute Y_LOC of MGT_GEN : label is c_MGT_MAP(index).y_loc;
+
+    attribute NUM_MGTS            : integer;              -- need it somewhere more handy actually...
+    attribute NUM_MGTS of MGT_GEN : label is c_NUM_MGTS;  -- make a copy of this handy for tcl
+
+  begin
+
+    MGT_GEN : entity xil_defaultlib.mgt_9g6_gty
+      port map (
+
+        -- drp
+        drpaddr_in => mgt_drp_i.drpaddr_in,
+        drpclk_in  => mgt_drp_i.drpclk_in,
+        drpdi_in   => mgt_drp_i.drpdi_in,
+        drpen_in   => mgt_drp_i.drpen_in,
+        drpwe_in   => mgt_drp_i.drpwe_in,
+        drpdo_out  => mgt_drp_o.drpdo_out,
+        drprdy_out => mgt_drp_o.drprdy_out,
+
+        -- multi-gigabit
+        gtyrxn_in(0)  => rxn_i,
+        gtyrxp_in(0)  => rxp_i,
+        gtytxn_out(0) => txn_o,
+        gtytxp_out(0) => txp_o,
+
+        gtwiz_userclk_tx_reset_in(0) => tx_resets_i.reset,
+
+        gtwiz_userdata_tx_in  => mgt_word_i,
+        gtwiz_userdata_rx_out => mgt_word_o,
+
+        -- clocks
+        rxusrclk2_in(0) => mgt_rxusrclk_i,
+        rxusrclk_in(0)  => mgt_rxusrclk_i,
+
+        txusrclk2_in(0) => mgt_txusrclk_i,
+        txusrclk_in(0)  => mgt_txusrclk_i,
+
+        gtwiz_userclk_rx_active_in(0) => mgt_rxusrclk_active_i,
+        gtwiz_userclk_tx_active_in(0) => mgt_txusrclk_active_i,
+
+        gtwiz_reset_clk_freerun_in(0) => clock,
+
+        gtwiz_reset_rx_pll_and_datapath_in(0) => rx_resets_i.reset_pll_and_datapath,
+        gtwiz_reset_tx_pll_and_datapath_in(0) => tx_resets_i.reset_pll_and_datapath,
+        gtwiz_reset_rx_datapath_in(0)         => rx_resets_i.reset_datapath,
+        gtwiz_reset_tx_datapath_in(0)         => tx_resets_i.reset_datapath,
+
+        -- buffer bypass
+        gtwiz_buffbypass_tx_reset_in(0)      => tx_resets_i.reset_bufbypass,
+        gtwiz_buffbypass_tx_start_user_in(0) => '0',
+        gtwiz_buffbypass_tx_done_out(0)      => status_o.buffbypass_tx_done_out,
+        gtwiz_buffbypass_tx_error_out(0)     => status_o.buffbypass_tx_error_out,
+
+        gtwiz_buffbypass_rx_reset_in(0)      => rx_resets_i.reset_bufbypass,
+        gtwiz_buffbypass_rx_start_user_in(0) => '0',
+        gtwiz_buffbypass_rx_done_out(0)      => status_o.buffbypass_rx_done_out,
+        gtwiz_buffbypass_rx_error_out(0)     => status_o.buffbypass_rx_error_out,
+
+        --
+        rxpmaresetdone_out(0)      => status_o.rx_pma_reset_done,
+        txpmaresetdone_out(0)      => status_o.tx_pma_reset_done,
+        gtwiz_reset_rx_done_out(0) => status_o.rx_reset_done,
+        gtwiz_reset_tx_done_out(0) => status_o.tx_reset_done,
+
+        gtrefclk0_in(0) => mgt_refclk_i,
+
+        gtpowergood_out(0)               => status_o.powergood,
+        gtwiz_reset_rx_cdr_stable_out(0) => status_o.rxcdr_stable,
+
+        gtwiz_reset_all_in(0) => reset_i,
+
+        txctrl0_in => txctrl0_in,
+        txctrl1_in => txctrl1_in,
+        txctrl2_in => txctrl2_in,
+
+        rxctrl0_out => rxctrl0_out,
+        rxctrl1_out => rxctrl1_out,
+        rxctrl2_out => rxctrl2_out,
+        rxctrl3_out => rxctrl3_out,
+
+        tx8b10ben_in => xilinx_one,
+        rx8b10ben_in => xilinx_one,
+
+        rxslide_in(0) => rx_slide_i,
+
+        rxoutclk_out => open,
+        txoutclk_out => open
+        );
+
+  end generate gty_gen;
+
+  nil_gen : if (gt_type = GT_NIL) generate
+    assert false report "GENERATING NIL link not supported" severity error;
+  end generate nil_gen;
 
 end Behavioral;
