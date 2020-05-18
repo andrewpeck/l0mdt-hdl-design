c28815c45d6a580ec7cea09a16513d71e9f9ba7a
GLdL <guillermo.jose.loustau.de.linares@cern.ch>
2020-05-18 21:30:43 +0200

    main pipeline done

--- mpl/src/mpl_csw.vhd
+++ mpl/src/mpl_csw.vhd
@@ -38,10 +38,41 @@ entity mpl_csw is
 end entity mpl_csw;
 
 architecture beh of mpl_csw is
+
+  signal slc_pl : pipelines_at(NUM_THREADS -1 downto 0);
   
 begin
+
+  V2R: for sl_i in NUM_THREADS - 1 downto 0 generate
+    slc_pl(sl_i) <= structify(i_pl2plcsw_av(sl_i));
+  end generate V2R;
+
+  MP2TF_CSW: process(clk)
+    variable sli_i      : integer := 0;
+    variable slo_found  : std_logic := '0';
+  begin
+    if rising_edge(clk) then
+      if Reset_b = '0' then
+        
+      else
+        for slo_i in NUM_THREADS -1 downto 0 loop
+          slo_found := '0';
+          for sli_i in NUM_THREADS -1 downto 0 loop
+            if ((slc_pl(sli_i).processed = '1') and (to_integer(unsigned(slc_pl(sli_i).process_ch)) = slo_i)) then
+              o_plcsw2tf_av(slo_i) <= i_pl2plcsw_av(sli_i);
+              slo_found := '1';
+            end if;
+          end loop;
+          if slo_found = '0' then
+            o_plcsw2tf_av(slo_i) <= nullify(o_plcsw2tf_av(slo_i));
+          end if;
+        end loop;
+        
+      end if;
+    end if;
+  end process MP2TF_CSW;
   
-  
+  -- 
   
 end architecture beh;
 
