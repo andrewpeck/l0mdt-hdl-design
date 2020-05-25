cc17599bcd0defa8a513959c06a4ca697fdd4b6f
GLdL <guillermo.jose.loustau.de.linares@cern.ch>
2020-05-25 14:02:02 +0200

    slope file created

---
+++ ucm/src/ucm_cvp_slope.vhd
@@ -0,0 +1,22 @@
+--------------------------------------------------------------------------------
+--  UMass , Physics Department
+--  Guillermo Loustau de Linares
+--  gloustau@cern.ch
+--------------------------------------------------------------------------------
+--  Project: ATLAS L0MDT Trigger 
+--  Module: slc vector processor
+--  Description:
+--
+--------------------------------------------------------------------------------
+--  Revisions:
+--      
+--------------------------------------------------------------------------------
+library ieee, shared_lib;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+library shared_lib;
+use shared_lib.config_pkg.all;
+use shared_lib.common_pkg.all;
+library ucm_lib;
+use ucm_lib.ucm_pkg.all;
