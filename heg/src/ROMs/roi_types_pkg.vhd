eb000fcbab21535cc442caecb1e1b2ad961993fe
GLdL <guillermo.jose.loustau.de.linares@cern.ch>
2020-06-07 11:08:40 +0200

    some dependencies correc5ted

--- heg/src/ROMs/roi_types_pkg.vhd
+++ heg/src/ROMs/roi_types_pkg.vhd
@@ -2,7 +2,7 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
--- library shared_lib;
+library shared_lib;
 -- use shared_lib.common_types_pkg.all;
 use shared_lib.common_constants_pkg.all;
 -- use shared_lib.config_pkg.all;
