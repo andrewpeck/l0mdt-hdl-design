10faae6335302da2473dfb9ede5891df4a2336f4
Andrew Peck <andrew.peck@cern.ch>
2020-07-21 19:18:34 -0400

    Make project.tcl and gitlab-ci identical to csf

--- Top/hal/hal.tcl
+++ Top/hal/hal.tcl
@@ -1,45 +1,27 @@
 #vivado
 ############# modify these to match project ################
 set BIN_FILE 1
-set USE_QUESTA_SIMULATOR 0
 
 ## FPGA and Vivado strategies and flows
 set FPGA xcku15p-ffva1760-2-e
-set SYNTH_STRATEGY "Vivado Synthesis Defaults"
+set SYNTH_STRATEGY "Flow_PerfOptimized_high"
 set SYNTH_FLOW "Vivado Synthesis 2019"
-set IMPL_STRATEGY "Vivado Implementation Defaults"
+set IMPL_STRATEGY "Performance_ExplorePostRoutePhysOpt"
 set IMPL_FLOW "Vivado Implementation 2019"
-
-### Set Vivado Runs Properties ###
-#
-# ATTENTION: The \ character must be the last one of each line
-#
-# The default Vivado run names are: synth_1 for synthesis and impl_1 for implementation.
-#
-# To find out the exact name and value of the property, use Vivado GUI to click on the checkbox you like.
-# This will make Vivado run the set_property command in the Tcl console.
-# Then copy and paste the name and the values from the Vivado Tcl console into the lines below.
+set SIMULATOR "Xsim"
 
 set PROPERTIES [dict create \
-    synth_1 [dict create \
-        STEPS.SYNTH_DESIGN.ARGS.ASSERT true \
-        STEPS.SYNTH_DESIGN.ARGS.RETIMING false \
-        STEPS.SYNTH_DESIGN.ARGS.FANOUT_LIMIT 500 \
-        ] \
-    impl_1 [dict create \
-        STEPS.PHYS_OPT_DESIGN.IS_ENABLED true \
-        STEPS.POST_ROUTE_PHYS_OPT_DESIGN.IS_ENABLED true \
-        STEPS.OPT_DESIGN.ARGS.DIRECTIVE Default \
-        STEPS.POST_ROUTE_PHYS_OPT_DESIGN.ARGS.DIRECTIVE Default \
-        ]\
-    ]
+            synth_1 [dict create \
+                STEPS.SYNTH_DESIGN.ARGS.FANOUT_LIMIT 600 \
+                STEPS.SYNTH_DESIGN.ARGS.RETIMING true \
+                ] \
+            impl_1 [dict create \
+                STEPS.OPT_DESIGN.ARGS.DIRECTIVE Default \
+                STEPS.POST_ROUTE_PHYS_OPT_DESIGN.ARGS.DIRECTIVE AggressiveExplore \
+                   ]\
+           ]
 ############################################################
-
-
-############################################################
-set SYNTH_TOP_MODULE  top_hal
-set SIMULATOR  xsim
 set DESIGN    "[file rootname [file tail [info script]]]"
-set PATH_REPO "[file normalize [file dirname [info script]]]/../../"
-
+set PATH_REPO "[file normalize [file dirname [info script]]]/../.."
 source $PATH_REPO/Hog/Tcl/create_project.tcl
+set_property default_lib work [current_project]
