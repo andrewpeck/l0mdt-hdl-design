9aba2cf4944896302e42693a89fec4347e55fbbf
Eric Hazen <hazen@bu.edu>
2020-07-28 14:36:42 -0400

    fighting with git

--- tools/yml2tb/test/stupid.sh
+++ tools/yml2tb/test/stupid.sh
@@ -1,5 +1,5 @@
 # GHDL=/usr/bin/ghdl
-GHDL=/home/hazen/src/ghdl-0.37/ghdl_mcode 
+GHDL=/home/hazen/src/ghdl-0.37/bin/ghdl
 AOPT="--ieee=synopsys"
 rm -f *.cf
 $GHDL -a $AOPT --work=stupid stupid_types.vhd
