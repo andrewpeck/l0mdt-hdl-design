b567f64693f23f943d82f2981c70200333b61e93
Guillermo <guillermo.jose.loustau.de.linares@cern.ch>
2020-09-15 18:09:34 +0200

    fixed: dataformat repo was not master, las updates in SS were not updated

--- shared/types/common_types_pkg.vhd
+++ shared/types/common_types_pkg.vhd
@@ -422,12 +422,12 @@ package body common_types_pkg is
     return y;
   end function vectorify;
   function vectorify(x: mdt_polmux_bus_at) return std_logic_vector is
-    variable y : std_logic_vector(x'length*43-1 downto 0);
+    variable y : std_logic_vector(x'length*42-1 downto 0);
     variable msb : integer := y'length-1;
   begin
     l: for i in x'range loop
-      y(msb downto msb-43) := vectorify(x(i));
-      msb := msb - 43 -1;
+      y(msb downto msb-42) := vectorify(x(i));
+      msb := msb - 42 -1;
     end loop l;
     return y;
   end function vectorify;
@@ -444,8 +444,8 @@ package body common_types_pkg is
     variable msb : integer := x'length-1;
   begin
     l: for i in y'range loop
-      y(i) := structify(x(msb downto msb-43));
-      msb := msb - 43 -1;
+      y(i) := structify(x(msb downto msb-42));
+      msb := msb - 42 -1;
     end loop l;
     return y;
   end function structify;
