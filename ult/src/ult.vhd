894928552d275b6de793e786bbe975f9c9a006aa
Andrew Peck <andrew.peck@cern.ch>
2020-10-17 10:00:27 -0400

    Whitespace changes

--- ult/src/ult.vhd
+++ ult/src/ult.vhd
@@ -37,7 +37,7 @@ use ctrl_lib.MPL_CTRL.all;
 
 entity ult is
   generic (
-    DUMMY       : boolean := false
+    DUMMY : boolean := false
     );
   port (
     -- clock and ttc
@@ -80,25 +80,25 @@ entity ult is
     i_extra_tar_hits  : in tar2hps_bus_avt (c_EN_TAR_HITS*c_HPS_NUM_MDT_CH_EXT -1 downto 0);
 
     -- Sector Logic Candidates
-    i_main_primary_slc        : in  slc_rx_bus_avt(2 downto 0);  -- is the main SL used
-    i_main_secondary_slc      : in  slc_rx_bus_avt(2 downto 0);  -- only used in the big endcap
-    i_plus_neighbor_slc       : in  slc_rx_rvt;
-    i_minus_neighbor_slc      : in  slc_rx_rvt;
+    i_main_primary_slc   : in slc_rx_bus_avt(2 downto 0);  -- is the main SL used
+    i_main_secondary_slc : in slc_rx_bus_avt(2 downto 0);  -- only used in the big endcap
+    i_plus_neighbor_slc  : in slc_rx_rvt;
+    i_minus_neighbor_slc : in slc_rx_rvt;
 
     -- Segments in from neighbor
-    plus_neighbor_segments_i  : in  sf2pt_bus_avt(c_NUM_SF_INPUTS - 1 downto 0);
-    minus_neighbor_segments_i : in  sf2pt_bus_avt(c_NUM_SF_INPUTS - 1 downto 0);
+    plus_neighbor_segments_i  : in sf2pt_bus_avt(c_NUM_SF_INPUTS - 1 downto 0);
+    minus_neighbor_segments_i : in sf2pt_bus_avt(c_NUM_SF_INPUTS - 1 downto 0);
 
     -- Array of DAQ data streams (e.g. 64 bit strams) to send to MGT
-    daq_streams_o             : out felix_stream_bus_avt (c_NUM_DAQ_STREAMS-1 downto 0);
+    daq_streams_o : out felix_stream_bus_avt (c_NUM_DAQ_STREAMS-1 downto 0);
 
     -- Segments Out to Neighbor
     plus_neighbor_segments_o  : out sf2pt_bus_avt(c_NUM_SF_OUTPUTS - 1 downto 0);
     minus_neighbor_segments_o : out sf2pt_bus_avt(c_NUM_SF_OUTPUTS - 1 downto 0);
 
     -- -- MUCTPI
-    MTC_o                     : out mtc_out_bus_avt(c_NUM_MTC-1 downto 0);
-    NSP_o                     : out mtc2nsp_bus_avt(c_NUM_NSP-1 downto 0);
+    MTC_o : out mtc_out_bus_avt(c_NUM_MTC-1 downto 0);
+    NSP_o : out mtc2nsp_bus_avt(c_NUM_NSP-1 downto 0);
     -- AXI Control
 
     sump : out std_logic
@@ -180,109 +180,109 @@ begin
         o_outer_tar_hits  => outer_tar_hits,
         o_extra_tar_hits  => extra_tar_hits
 
-      );
+        );
 
     UCM : entity work.candidate_manager
-    port map (
-      -- clock, control, and monitoring
-      clock_and_control       => clock_and_control,  --
-      ttc_commands            => ttc_commands,       --
-      ctrl                    => ucm_ctrl,
-      mon                     => ucm_mon,
-      -- candidates in from hal
-      i_slc_data_mainA_av     => i_main_primary_slc,
-      i_slc_data_mainB_av     => i_main_secondary_slc,
-      i_slc_data_neighborA_v => i_plus_neighbor_slc,
-      i_slc_data_neighborB_v => i_minus_neighbor_slc,
-      -- outputs to ucm
-      o_uCM2hps_inn_av        => inner_slc_to_hts,
-      o_uCM2hps_mid_av        => middle_slc_to_hts,
-      o_uCM2hps_out_av        => outer_slc_to_hts,
-      o_uCM2hps_ext_av        => extra_slc_to_hts,
-      -- pipeline
-      o_uCM2pl_av             => ucm2pl_av
-    );
+      port map (
+        -- clock, control, and monitoring
+        clock_and_control      => clock_and_control,  --
+        ttc_commands           => ttc_commands,       --
+        ctrl                   => ucm_ctrl,
+        mon                    => ucm_mon,
+        -- candidates in from hal
+        i_slc_data_mainA_av    => i_main_primary_slc,
+        i_slc_data_mainB_av    => i_main_secondary_slc,
+        i_slc_data_neighborA_v => i_plus_neighbor_slc,
+        i_slc_data_neighborB_v => i_minus_neighbor_slc,
+        -- outputs to ucm
+        o_uCM2hps_inn_av       => inner_slc_to_hts,
+        o_uCM2hps_mid_av       => middle_slc_to_hts,
+        o_uCM2hps_out_av       => outer_slc_to_hts,
+        o_uCM2hps_ext_av       => extra_slc_to_hts,
+        -- pipeline
+        o_uCM2pl_av            => ucm2pl_av
+        );
 
 
     H2S : entity work.hits_to_segments
-    port map (
-      -- clock, control, and monitoring
-      clock_and_control         => clock_and_control,
-      ttc_commands              => ttc_commands,
-      ctrl                      => h2s_ctrl,
-      mon                       => h2s_mon,
-      -- inputs from hal
-      i_inn_tar_hits            => inner_tar_hits,
-      i_mid_tar_hits            => middle_tar_hits,
-      i_out_tar_hits            => outer_tar_hits,
-      i_ext_tar_hits            => extra_tar_hits,
-      -- Sector Logic Candidates from uCM
-      i_inn_slc                 => inner_slc_to_hts,
-      i_mid_slc                 => middle_slc_to_hts,
-      i_out_slc                 => outer_slc_to_hts,
-      i_ext_slc                 => extra_slc_to_hts,
-      -- Segments Out to pt calculation
-      o_inn_segments            => inner_segments_to_pt,
-      o_mid_segments            => middle_segments_to_pt,
-      o_out_segments            => outer_segments_to_pt,
-      o_ext_segments            => extra_segments_to_pt,
-      -- Segment outputs to HAL
-      plus_neighbor_segments_o  => plus_neighbor_segments_o,
-      minus_neighbor_segments_o => minus_neighbor_segments_o
-    );
+      port map (
+        -- clock, control, and monitoring
+        clock_and_control         => clock_and_control,
+        ttc_commands              => ttc_commands,
+        ctrl                      => h2s_ctrl,
+        mon                       => h2s_mon,
+        -- inputs from hal
+        i_inn_tar_hits            => inner_tar_hits,
+        i_mid_tar_hits            => middle_tar_hits,
+        i_out_tar_hits            => outer_tar_hits,
+        i_ext_tar_hits            => extra_tar_hits,
+        -- Sector Logic Candidates from uCM
+        i_inn_slc                 => inner_slc_to_hts,
+        i_mid_slc                 => middle_slc_to_hts,
+        i_out_slc                 => outer_slc_to_hts,
+        i_ext_slc                 => extra_slc_to_hts,
+        -- Segments Out to pt calculation
+        o_inn_segments            => inner_segments_to_pt,
+        o_mid_segments            => middle_segments_to_pt,
+        o_out_segments            => outer_segments_to_pt,
+        o_ext_segments            => extra_segments_to_pt,
+        -- Segment outputs to HAL
+        plus_neighbor_segments_o  => plus_neighbor_segments_o,
+        minus_neighbor_segments_o => minus_neighbor_segments_o
+        );
 
     pipeline_inst : entity ult_lib.pipeline
-    port map (
-      -- clock, control, and monitoring
-      clock_and_control => clock_and_control,
-      ttc_commands      => ttc_commands,
-      ctrl              => mpl_ctrl,
-      mon               => mpl_mon,
+      port map (
+        -- clock, control, and monitoring
+        clock_and_control => clock_and_control,
+        ttc_commands      => ttc_commands,
+        ctrl              => mpl_ctrl,
+        mon               => mpl_mon,
 
-      -- Sector Logic Candidates from uCM
-      i_ucm2pl_av => ucm2pl_av,
+        -- Sector Logic Candidates from uCM
+        i_ucm2pl_av => ucm2pl_av,
 
-      -- Sector Logic Candidates to pt calculation
-      o_pl2pt_av  => pl2pt_av,
-      -- Sector Logic Candidates to mTC
-      o_pl2mtc_av => pl2mtc_av
-    );
+        -- Sector Logic Candidates to pt calculation
+        o_pl2pt_av  => pl2pt_av,
+        -- Sector Logic Candidates to mTC
+        o_pl2mtc_av => pl2mtc_av
+        );
 
     ptcalc_inst : entity work.ptcalc
-    port map (
-      -- clock, control, and monitoring
-      clock_and_control         => clock_and_control,
-      ttc_commands              => ttc_commands,
-      ctrl                      => tf_ctrl,
-      mon                       => tf_mon,
-      --  segments from neighbors
-      plus_neighbor_segments_i  => plus_neighbor_segments_i,
-      minus_neighbor_segments_i => minus_neighbor_segments_i,
-      -- segments from hps
-      inner_segments_i          => inner_segments_to_pt,
-      middle_segments_i         => middle_segments_to_pt,
-      outer_segments_i          => outer_segments_to_pt,
-      extra_segments_i          => extra_segments_to_pt,
-      -- from pipeline
-      i_pl2pt_av                => pl2pt_av,
-      -- to mtc
-      o_pt2mtc                  => pt2mtc_av
-    );
+      port map (
+        -- clock, control, and monitoring
+        clock_and_control         => clock_and_control,
+        ttc_commands              => ttc_commands,
+        ctrl                      => tf_ctrl,
+        mon                       => tf_mon,
+        --  segments from neighbors
+        plus_neighbor_segments_i  => plus_neighbor_segments_i,
+        minus_neighbor_segments_i => minus_neighbor_segments_i,
+        -- segments from hps
+        inner_segments_i          => inner_segments_to_pt,
+        middle_segments_i         => middle_segments_to_pt,
+        outer_segments_i          => outer_segments_to_pt,
+        extra_segments_i          => extra_segments_to_pt,
+        -- from pipeline
+        i_pl2pt_av                => pl2pt_av,
+        -- to mtc
+        o_pt2mtc                  => pt2mtc_av
+        );
 
     mtc_builder_inst : entity work.mtc_builder
-    port map (
-      -- clock, control, and monitoring
-      clock_and_control => clock_and_control,
-      ttc_commands      => ttc_commands,
-      ctrl              => mtc_ctrl,
-      mon               => mtc_mon,
-      --  inputs
-      i_ptcalc          => pt2mtc_av,
-      i_pl2mtc          => pl2mtc_av,
-      -- outputs
-      o_mtc             => mtc_o,
-      o_nsp             => nsp_o
-    );
+      port map (
+        -- clock, control, and monitoring
+        clock_and_control => clock_and_control,
+        ttc_commands      => ttc_commands,
+        ctrl              => mtc_ctrl,
+        mon               => mtc_mon,
+        --  inputs
+        i_ptcalc          => pt2mtc_av,
+        i_pl2mtc          => pl2mtc_av,
+        -- outputs
+        o_mtc             => mtc_o,
+        o_nsp             => nsp_o
+        );
 
     daq_inst : entity work.daq
       generic map(DELAY => 9600, memory_type => "ultra")
@@ -292,16 +292,16 @@ begin
         ttc_commands      => ttc_commands,
         ctrl              => daq_ctrl,
         mon               => daq_mon,
-        
+
         -- TDC Hits from Polmux
         i_inner_tdc_hits  => inner_tdc_hits,
         i_middle_tdc_hits => middle_tdc_hits,
         i_outer_tdc_hits  => outer_tdc_hits,
         i_extra_tdc_hits  => extra_tdc_hits,
-        
+
         -- Tracks from MTC
         -- ???
-        
+
         -- Array of DAQ data streams (e.g. 64 bit streams) to send to MGT
         daq_streams_o => daq_streams_o);
 
