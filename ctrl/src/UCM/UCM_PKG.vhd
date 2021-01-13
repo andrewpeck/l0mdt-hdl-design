43b9aac40d28a5e56a0afefb8dd692691a3cc6e4
GLdL <guillermo.jose.loustau.de.linares@cern.ch>
2021-01-13 16:39:31 +0100

    fixed ucm xml

--- ctrl/src/UCM/UCM_PKG.vhd
+++ ctrl/src/UCM/UCM_PKG.vhd
@@ -46,28 +46,53 @@ package UCM_CTRL is
                                                                      OVERRIDE => '0',
                                                                      VALUE => x"0000"
                                                                     );
-  type UCM_CHAMBER_Z0_CHAMBER_Z0_CTRL_t is record
+  type UCM_DP_CHAMB_Z0_DP_CHAMB_Z0_CALC_WR_CTRL_t is record
     VALUE                      :std_logic_vector(15 downto 0);  -- New value to write
     ADDR                       :std_logic_vector( 7 downto 0);  -- position or chamber to r/w value
     WR_EN                      :std_logic;                      -- Write Enable
-  end record UCM_CHAMBER_Z0_CHAMBER_Z0_CTRL_t;
-  type UCM_CHAMBER_Z0_CHAMBER_Z0_CTRL_t_ARRAY is array(0 to 3) of UCM_CHAMBER_Z0_CHAMBER_Z0_CTRL_t;
-
-  constant DEFAULT_UCM_CHAMBER_Z0_CHAMBER_Z0_CTRL_t : UCM_CHAMBER_Z0_CHAMBER_Z0_CTRL_t := (
-                                                                                           WR_EN => '0',
-                                                                                           ADDR => (others => '0'),
-                                                                                           VALUE => (others => '0')
-                                                                                          );
-  type UCM_CHAMBER_Z0_CTRL_t is record
-    CHAMBER_Z0                 :UCM_CHAMBER_Z0_CHAMBER_Z0_CTRL_t_ARRAY;
-  end record UCM_CHAMBER_Z0_CTRL_t;
+  end record UCM_DP_CHAMB_Z0_DP_CHAMB_Z0_CALC_WR_CTRL_t;
 
 
-  constant DEFAULT_UCM_CHAMBER_Z0_CTRL_t : UCM_CHAMBER_Z0_CTRL_t := (
-                                                                     CHAMBER_Z0 => (others => DEFAULT_UCM_CHAMBER_Z0_CHAMBER_Z0_CTRL_t )
-                                                                    );
+  constant DEFAULT_UCM_DP_CHAMB_Z0_DP_CHAMB_Z0_CALC_WR_CTRL_t : UCM_DP_CHAMB_Z0_DP_CHAMB_Z0_CALC_WR_CTRL_t := (
+                                                                                                               WR_EN => '0',
+                                                                                                               ADDR => (others => '0'),
+                                                                                                               VALUE => (others => '0')
+                                                                                                              );
+  type UCM_DP_CHAMB_Z0_DP_CHAMB_Z0_CALC_RD_MON_t is record
+    VALUE                      :std_logic_vector(15 downto 0);  -- Read Value id position selected
+    RST_REQ                    :std_logic;                      -- Reset request after changes in the mem
+  end record UCM_DP_CHAMB_Z0_DP_CHAMB_Z0_CALC_RD_MON_t;
+
+
+  type UCM_DP_CHAMB_Z0_DP_CHAMB_Z0_MON_t is record
+    CALC_RD                    :UCM_DP_CHAMB_Z0_DP_CHAMB_Z0_CALC_RD_MON_t;
+  end record UCM_DP_CHAMB_Z0_DP_CHAMB_Z0_MON_t;
+  type UCM_DP_CHAMB_Z0_DP_CHAMB_Z0_MON_t_ARRAY is array(0 to 3) of UCM_DP_CHAMB_Z0_DP_CHAMB_Z0_MON_t;
+
+  type UCM_DP_CHAMB_Z0_DP_CHAMB_Z0_CTRL_t is record
+    CALC_WR                    :UCM_DP_CHAMB_Z0_DP_CHAMB_Z0_CALC_WR_CTRL_t;
+  end record UCM_DP_CHAMB_Z0_DP_CHAMB_Z0_CTRL_t;
+  type UCM_DP_CHAMB_Z0_DP_CHAMB_Z0_CTRL_t_ARRAY is array(0 to 3) of UCM_DP_CHAMB_Z0_DP_CHAMB_Z0_CTRL_t;
+
+  constant DEFAULT_UCM_DP_CHAMB_Z0_DP_CHAMB_Z0_CTRL_t : UCM_DP_CHAMB_Z0_DP_CHAMB_Z0_CTRL_t := (
+                                                                                               CALC_WR => DEFAULT_UCM_DP_CHAMB_Z0_DP_CHAMB_Z0_CALC_WR_CTRL_t
+                                                                                              );
+  type UCM_DP_CHAMB_Z0_MON_t is record
+    DP_CHAMB_Z0                :UCM_DP_CHAMB_Z0_DP_CHAMB_Z0_MON_t_ARRAY;
+  end record UCM_DP_CHAMB_Z0_MON_t;
+
+
+  type UCM_DP_CHAMB_Z0_CTRL_t is record
+    DP_CHAMB_Z0                :UCM_DP_CHAMB_Z0_DP_CHAMB_Z0_CTRL_t_ARRAY;
+  end record UCM_DP_CHAMB_Z0_CTRL_t;
+
+
+  constant DEFAULT_UCM_DP_CHAMB_Z0_CTRL_t : UCM_DP_CHAMB_Z0_CTRL_t := (
+                                                                       DP_CHAMB_Z0 => (others => DEFAULT_UCM_DP_CHAMB_Z0_DP_CHAMB_Z0_CTRL_t )
+                                                                      );
   type UCM_MON_t is record
     STATUS                     :UCM_STATUS_MON_t;
+    DP_CHAMB_Z0                :UCM_DP_CHAMB_Z0_MON_t;
   end record UCM_MON_t;
 
 
@@ -75,14 +100,14 @@ package UCM_CTRL is
     ACTIONS                    :UCM_ACTIONS_CTRL_t;
     CONFIGS                    :UCM_CONFIGS_CTRL_t;
     SECTOR_PHI                 :UCM_SECTOR_PHI_CTRL_t;
-    CHAMBER_Z0                 :UCM_CHAMBER_Z0_CTRL_t;
+    DP_CHAMB_Z0                :UCM_DP_CHAMB_Z0_CTRL_t;
   end record UCM_CTRL_t;
 
 
   constant DEFAULT_UCM_CTRL_t : UCM_CTRL_t := (
+                                               DP_CHAMB_Z0 => DEFAULT_UCM_DP_CHAMB_Z0_CTRL_t,
                                                CONFIGS => DEFAULT_UCM_CONFIGS_CTRL_t,
                                                SECTOR_PHI => DEFAULT_UCM_SECTOR_PHI_CTRL_t,
-                                               CHAMBER_Z0 => DEFAULT_UCM_CHAMBER_Z0_CTRL_t,
                                                ACTIONS => DEFAULT_UCM_ACTIONS_CTRL_t
                                               );
 
