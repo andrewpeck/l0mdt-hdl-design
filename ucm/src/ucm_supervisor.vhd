e21d6fb9fa55281c155032cfc4e13c76b816c041
Guillermo <guillermo.jose.loustau.de.linares@cern.ch>
2021-01-29 09:59:01 +0100

    new ucm general manager

---
+++ ucm/src/ucm_supervisor.vhd
@@ -0,0 +1,46 @@
+--------------------------------------------------------------------------------
+--  UMass , Physics Department
+--  Guillermo Loustau de Linares
+--  gloustau@cern.ch
+--------------------------------------------------------------------------------
+--  Project: ATLAS L0MDT Trigger 
+--  Module: UCM general supervisor
+--  Description:
+--
+--------------------------------------------------------------------------------
+--  Revisions:
+--      
+--------------------------------------------------------------------------------
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+use ieee.numeric_std_unsigned.all;
+use ieee.std_logic_misc.all;
+
+library shared_lib;
+use shared_lib.common_ieee_pkg.all;
+use shared_lib.l0mdt_constants_pkg.all;
+use shared_lib.l0mdt_dataformats_pkg.all;
+use shared_lib.common_constants_pkg.all;
+use shared_lib.common_types_pkg.all;
+use shared_lib.config_pkg.all;
+ 
+library ucm_lib;
+use ucm_lib.ucm_pkg.all;
+
+entity ucm_supervisor is
+  port (
+    clk                 : in std_logic;
+    rst                 : in std_logic;
+    glob_en             : in std_logic;
+    --
+  );
+end entity ucm_supervisor;
+
+architecture beh of ucm_supervisor is
+  
+begin
+  
+  
+  
+end architecture beh;
