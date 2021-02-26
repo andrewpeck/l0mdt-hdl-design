182655a41b12fbc6624cdaa4f8bd5b979b8445b9
Guillermo <guillermo.jose.loustau.de.linares@cern.ch>
2021-02-26 11:32:13 +0100

    added hps supervisor

---
+++ UserLogic/hps/src/hps_supervisor.vhd
@@ -0,0 +1,37 @@
+--------------------------------------------------------------------------------
+--  UMass , Physics Department
+--  Guillermo Loustau de Linares
+--  gloustau@cern.ch
+--------------------------------------------------------------------------------
+--  Project: ATLAS L0MDT Trigger 
+--  Module: 
+--  Description:
+--  
+--------------------------------------------------------------------------------
+--  Revisions:
+--      
+--------------------------------------------------------------------------------
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+library shared_lib;
+use shared_lib.common_ieee_pkg.all;
+use shared_lib.l0mdt_constants_pkg.all;
+use shared_lib.l0mdt_dataformats_pkg.all;
+use shared_lib.common_constants_pkg.all;
+use shared_lib.common_types_pkg.all;
+use shared_lib.config_pkg.all;
+-- use shared_lib.vhdl2008_functions_pkg.all;
+use shared_lib.detector_param_pkg.all;
+
+library hp_lib;
+use hp_lib.hp_pkg.all;
+library heg_lib;
+use heg_lib.heg_pkg.all;
+library hps_lib;
+use hps_lib.hps_pkg.all;
+
+library ctrl_lib;
+use ctrl_lib.H2S_CTRL.all;
