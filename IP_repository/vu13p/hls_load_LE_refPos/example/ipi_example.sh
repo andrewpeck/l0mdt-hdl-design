80a97875a44344c1ad9f87ffb08b721540be689d
Davide Cieri <davide.cieri@cern.ch>
2021-03-29 10:35:53 +0200

    FEATURE: Dedicated KU15P and VU13P ip repository folders

---
+++ IP_repository/vu13p/hls_load_LE_refPos/example/ipi_example.sh
@@ -0,0 +1,6 @@
+# ==============================================================
+# Vivado(TM) HLS - High-Level Synthesis from C, C++ and SystemC v2019.2.1 (64-bit)
+# Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
+# ==============================================================
+
+/opt/tools/Xilinx/v2019.2/Vivado/2019.2/bin/vivado  -notrace -mode batch -source ipi_example.tcl -tclargs xcvu13p-flga2577-1-e ../xilinx_com_hls_load_LE_refPos_1_0.zip
