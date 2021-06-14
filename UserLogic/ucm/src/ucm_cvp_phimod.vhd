1b9ede1648d35e80384ab5e66a908bf2fe0930f9
GLdL <guillermo.jose.loustau.de.linares@cern.ch>
2021-06-14 17:34:17 +0200

    fix len error

--- UserLogic/ucm/src/ucm_cvp_phimod.vhd
+++ UserLogic/ucm/src/ucm_cvp_phimod.vhd
@@ -62,7 +62,7 @@ architecture beh of ucm_cvp_phimod is
 
   signal sphi_buff : integer;-- := 0;--unsigned(18 -1 downto 0);
   -- signal sphi : unsigned(SLC_COMMON_POSPHI_LEN -1 downto 0);
-  signal phicenter : unsigned(SLC_COMMON_POSPHI_LEN - 1 downto 0) := get_sector_phi_center(c_SECTOR_ID);
+  signal phicenter : unsigned(SLC_COMMON_POSPHI_LEN - 1 downto 0) := get_sector_phi_center(c_SECTOR_ID,SLC_COMMON_POSPHI_LEN);
   -- signal phicenter_Default : unsigned(SLC_COMMON_POSPHI_LEN - 1 downto 0) := get_sector_phi_center(c_SECTOR_ID);
 
   constant reschanger : integer := integer((1024.0 * SLC_COMMON_POSPHI_MULT)/UCM2PL_PHIMOD_MULT);
