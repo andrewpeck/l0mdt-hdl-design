ff9bb8108ba07106e18bd6c2f817d3b0ef8d30ff
Priya Sundararajan <priya.sundararajan@cern.ch>
2021-06-22 12:24:05 -0700

    IP_repository unused , removing

--- IP_repository/ku15p/hls_compute_r_bins_64/example/ipi_example.sh
+++
@@ -1,6 +0,0 @@
-# ==============================================================
-# Vivado(TM) HLS - High-Level Synthesis from C, C++ and SystemC v2019.2.1 (64-bit)
-# Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
-# ==============================================================
-
-/opt/tools/Xilinx/v2019.2/Vivado/2019.2/bin/vivado  -notrace -mode batch -source ipi_example.tcl -tclargs xcku15p-ffva1760-2-e ../xilinx_com_hls_compute_r_bins_64_1_0.zip
