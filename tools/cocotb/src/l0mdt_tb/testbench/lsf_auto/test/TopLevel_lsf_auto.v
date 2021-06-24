7e1eb99215177644af0571cad949e543acc36d68
Kosta Ntekas <kostas.ntekas@cern.ch>
2021-06-24 04:53:48 -0700

    LSF cocoTB working again

--- tools/cocotb/src/l0mdt_tb/testbench/lsf_auto/test/TopLevel_lsf_auto.v
+++ tools/cocotb/src/l0mdt_tb/testbench/lsf_auto/test/TopLevel_lsf_auto.v
@@ -61,7 +61,11 @@ module TopLevel_lsf_auto #(
        for(genvar i = 0; i < 2; i++)
          begin:input_spybuffers
             SpyBuffer #(
-			.DATA_WIDTH(DATA_WIDTH-1),
+			.DATA_WIDTH_A(DATA_WIDTH-1),
+			.DATA_WIDTH_B(DATA_WIDTH-1),
+            .SPY_MEM_WIDTH_A(7),
+            .SPY_MEM_WIDTH_B(7),
+            .EL_MEM_WIDTH_A(7),
 			.FC_FIFO_WIDTH(FIFO_DEPTH),
 			.PASSTHROUGH(1)
 			) spybuffer (
@@ -115,7 +119,11 @@ module TopLevel_lsf_auto #(
       for(genvar i = 0; i < 1; i++)
         begin:output_spybuffers
            SpyBuffer #(
-		       .DATA_WIDTH(DATA_WIDTH-1),
+		       .DATA_WIDTH_A(DATA_WIDTH-1),
+			   .DATA_WIDTH_B(DATA_WIDTH-1),
+               .SPY_MEM_WIDTH_A(7),
+               .SPY_MEM_WIDTH_B(7),
+               .EL_MEM_WIDTH_A(7),
 		       .FC_FIFO_WIDTH(FIFO_DEPTH),
 		       .PASSTHROUGH(1)
                        ) spybuffer (
