3e17eba56bb2de25115f21c802e8336ec799c3ec
Andrew Peck <andrew.peck@cern.ch>
2021-08-09 11:22:57 -0400

    Use ini file reader instead of regex

--- Top/base_l0mdt/get_fpga_name.tcl
+++ Top/base_l0mdt/get_fpga_name.tcl
@@ -1,2 +1,8 @@
-set FPGA [exec sed -n s|\\s*\\(PART\\)\\s*=\\s*\\(\[0-9,A-z\]*\\)|\\2|p "[file dirname [info script]]/hog.conf"]
+package require inifile 0.2.3
+source "[file dirname [info script]]/../../Hog/Tcl/hog.tcl"
+
+set file_name "[file dirname [info script]]/hog.conf"
+
+set FPGA [dict get [dict get [ReadConf $file_name] main] PART]
+
 regexp {xc([0-9A-z]*)} $FPGA match fpga_shortname
