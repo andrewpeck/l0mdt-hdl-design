57005e23bced8454e4d50a52594d94e1f43e73dc
Davide Cieri <davide.cieri@cern.ch>
2021-08-11 16:27:17 +0200

    FEATURE: Updating SYNTH and IMPL flows to 2020.2

--- Top/hps/hps.tcl
+++ Top/hps/hps.tcl
@@ -5,21 +5,21 @@ set USE_QUESTA_SIMULATOR 0
 ## FPGA and Vivado strategies and flows
 set FPGA xcku15p-ffva1760-2-e
 set SYNTH_STRATEGY "Flow_PerfOptimized_high"
-set SYNTH_FLOW "Vivado Synthesis 2019"
+set SYNTH_FLOW "Vivado Synthesis 2020"
 set IMPL_STRATEGY "Performance_ExplorePostRoutePhysOpt"
-set IMPL_FLOW "Vivado Implementation 2019"
+set IMPL_FLOW "Vivado Implementation 2020"
 set SIMULATOR "Xsim"
 
 set PROPERTIES [dict create \
-            synth_1 [dict create \
-                STEPS.SYNTH_DESIGN.ARGS.FANOUT_LIMIT 600 \
-                STEPS.SYNTH_DESIGN.ARGS.RETIMING false \
-                ] \
-            impl_1 [dict create \
-                STEPS.OPT_DESIGN.ARGS.DIRECTIVE Default \
-                STEPS.POST_ROUTE_PHYS_OPT_DESIGN.ARGS.DIRECTIVE AggressiveExplore \
-                   ]\
-           ]
+    synth_1 [dict create \
+    STEPS.SYNTH_DESIGN.ARGS.FANOUT_LIMIT 600 \
+    STEPS.SYNTH_DESIGN.ARGS.RETIMING false \
+] \
+impl_1 [dict create \
+STEPS.OPT_DESIGN.ARGS.DIRECTIVE Default \
+STEPS.POST_ROUTE_PHYS_OPT_DESIGN.ARGS.DIRECTIVE AggressiveExplore \
+]\
+]
 ############################################################
 set DESIGN    "[file rootname [file tail [info script]]]"
 set PATH_REPO "[file normalize [file dirname [info script]]]/../.."
