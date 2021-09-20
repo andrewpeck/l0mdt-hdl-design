79a1bfe669e62e3c785dc64ee8fe3d978159484f
Priya Sundararajan <Priya.Sundararajan@cern.ch>
2021-09-20 08:05:49 -0700

    Adding testcase for hps - inner station  - Not working yet

---
+++ tools/cocotb/src/l0mdt_tb/testbench/hps/test/hps_top_tb.vhd
@@ -0,0 +1,107 @@
+--------------------------------------------------------------------------------
+--  UMass , Physics Department
+--  Guillermo Loustau de Linares
+--  gloustau@cern.ch
+--------------------------------------------------------------------------------
+--  Project: ATLAS L0MDT Trigger
+--  Module:
+--  Description:
+--
+--------------------------------------------------------------------------------
+--  Revisions:
+--
+--------------------------------------------------------------------------------
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+library shared_lib;
+use shared_lib.common_ieee_pkg.all;
+use shared_lib.l0mdt_constants_pkg.all;
+use shared_lib.l0mdt_dataformats_pkg.all;
+use shared_lib.common_constants_pkg.all;
+use shared_lib.common_types_pkg.all;
+use shared_lib.config_pkg.all;
+
+library hp_lib;
+use hp_lib.hp_pkg.all;
+library heg_lib;
+use heg_lib.heg_pkg.all;
+library hps_lib;
+use hps_lib.hps_pkg.all;
+
+library ctrl_lib;
+use ctrl_lib.H2S_CTRL.all;
+
+entity hps_top_tb is
+  generic(
+    -- mdt type
+    -- type mdt_type;
+    -- parameters
+    g_STATION_RADIUS    : integer := 0;  --station
+    g_HPS_NUM_MDT_CH     : integer := 6
+  );
+  port (
+    clk                 : in std_logic;
+    rst                 : in std_logic;
+    glob_en             : in std_logic := '1';
+
+    -- SLc
+    i_uCM2hps_av        : in ucm2hps_bus_avt(c_NUM_THREADS -1 downto 0);
+    -- MDT hit
+    -- i_mdt_polmux_av    : in tar2hps_avt(g_HPS_NUM_MDT_CH -1 downto 0);
+    i_mdt_tar_av        : in tar2hps_bus_avt(g_HPS_NUM_MDT_CH -1 downto 0);
+    -- to pt calc
+    o_sf2pt_av          : out sf2pt_bus_avt(c_NUM_THREADS -1 downto 0)
+  );
+end entity hps_top_tb;
+
+architecture beh of hps_top_tb is
+    -- control
+    signal ctrl              :  H2S_HPS_CTRL_t;
+    signal mon               :  H2S_HPS_MON_t;
+    signal ttc_commands      : l0mdt_ttc_rt;
+    signal ctrl_len : natural;
+    signal ctrl_v : std_logic_vector(len(ctrl)-1 downto 0) := (others => '0');
+    signal mon_v  : std_logic_vector(len(mon)-1 downto 0);
+    -- control
+
+  -- signal mdt_polmux_data_av : hps_mdt_input_avt(g_HPS_NUM_MDT_CH -1 downto 0)
+
+begin
+   ctrl_len <= len(ctrl);
+   ctrl_v(len(ctrl)-1 downto len(ctrl)-12 ) <= x"03e";
+
+  -- IN_GEN : for hp_i in g_HPS_NUM_MDT_CH downto 0 generate
+  --   mdt_polmux_data_av(hp_i).polmux <= i_mdt_polmux_av(hp_i);
+  --   mdt_polmux_data_av(hp_i).tar <= i_mdt_tar_av(hp_i);
+  -- end generate;
+
+  HPS : entity hps_lib.hps
+    generic map(
+      -- mdt type
+      -- mdt_type_av          => mdt_polmux_data_avt(g_HPS_NUM_MDT_CH -1 downto 0),
+      -- g_SIM_nBUILD         => '1',
+      -- parameters
+      g_STATION_RADIUS    => g_STATION_RADIUS,
+      g_HPS_NUM_MDT_CH     => g_HPS_NUM_MDT_CH
+    )
+    port map(
+      clk                 => clk,
+      rst                 => rst,
+      glob_en             => glob_en,
+
+      ctrl_v => ctrl_v,
+      mon_v => mon_v,
+
+      -- SLc
+      i_uCM2hps_av        => i_uCM2hps_av,
+      -- MDT hit
+      i_mdt_tar_av        => i_mdt_tar_av,
+      -- to pt calc
+      o_sf2pt_av          => o_sf2pt_av
+    );
+
+
+end beh;
