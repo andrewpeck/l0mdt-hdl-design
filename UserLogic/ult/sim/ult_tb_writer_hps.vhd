0faf973ee3da2d16d1ba135ea1c31e6bc430df81
GLdL <guillermo.jose.loustau.de.linares@cern.ch>
2021-10-04 15:37:04 +0200

    experimenting  with libs

--- UserLogic/ult/sim/ult_tb_writer_hps.vhd
+++ UserLogic/ult/sim/ult_tb_writer_hps.vhd
@@ -29,10 +29,13 @@ use shared_lib.config_pkg.all;
 -- use shared_lib.vhdl2008_functions_pkg.all;
 use shared_lib.detector_param_pkg.all;
 
+use shared_lib.vhdl_tb_utils_pkg.all;
+
+
 library project_lib;
 use project_lib.ult_tb_sim_pkg.all;
 use project_lib.ult_tb_sim_cstm_pkg.all;
-use project_lib.vhdl_tb_utils_pkg.all;
+-- use project_lib.vhdl_tb_utils_pkg.all;
 use project_lib.vhdl_textio_csv_pkg.all;
 
 library ult_lib;
