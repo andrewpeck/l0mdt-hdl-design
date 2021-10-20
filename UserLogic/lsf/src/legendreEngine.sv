2a3ef9352bae75ec00ead837d9a90bd27b114a06
Kosta Ntekas <kostas.ntekas@cern.ch>
2021-10-20 14:56:13 -0700

    added seg_quality for LSF

--- UserLogic/lsf/src/legendreEngine.sv
+++ UserLogic/lsf/src/legendreEngine.sv
@@ -853,7 +853,7 @@ end
 	     le_output_vld       <= 0;
 	     le_tb_output        <= 0;
 	     le_tb_output_vld    <= 0;
-	     sf_segquality       <= 0;
+	     //sf_segquality       <= 0;
   	     histogram_reset_n   <= 1'h0;
 	  end
 	else
@@ -1343,7 +1343,8 @@ hls_find_max_bin find_max_bin_inst(
 				  .res_max_bin_theta_V(res_max_bin_theta),
 				  .res_max_bin_theta_V_ap_vld(res_max_bin_theta_vld),
 				  .res_max_bin_r_V(res_max_bin_r),
-				  .res_max_bin_r_V_ap_vld(res_max_bin_r_vld)
+				  .res_max_bin_r_V_ap_vld(res_max_bin_r_vld),
+				  .segquality (sf_segquality)
 				  );
 end
 else
