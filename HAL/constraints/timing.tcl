511ae43410ca51d1bcb18226408218eb01e9aaaf
Andrew Peck <andrew.peck@cern.ch>
2021-10-21 14:38:33 -0400

    hal: add some timing exceptions

--- HAL/constraints/timing.tcl
+++ HAL/constraints/timing.tcl
@@ -15,7 +15,29 @@ foreach refclk [get_clocks refclk*] {
 # clock domain crossing
 ################################################################################
 
-set_false_path -from [get_pins {top_control_inst/*/sys_reseter/*/*/C}]
+# this timing constraint applies to the OOC synthesized BD, so if you try to set
+# this during synthesis vivado barfs that the pin can't be found, so we need the
+# -quiet flag
+set_false_path -quiet -from [get_pins {top_control_inst/*/sys_reseter/*/*/C}]
+
+################################################################################
+# Transitions to/from the AXI clocks are asynchronous
+################################################################################
+
+set_clock_groups -group [get_clocks clock_100] -asynchronous
+set_clock_groups -group [get_clocks axi_clk] -asynchronous
+
+################################################################################
+# FIXME: this is a kludgey workaround... the SL data needs to be fixed latency,
+# so we need to have a sensible way to clock domain crossings from the SL clock
+# received domain to the logic clock
+################################################################################
+
+set_max_delay -datapath_only 5 \
+    -from [get_pins \
+               {top_hal/*sector_logic*/*rx_packet_former*/*/C}] \
+    -to [get_pins \
+             {top_hal/*sector_logic*/*sl_rx_data*/D}]
 
 ################################################################################
 # Uncomment to disable all logic trimming
