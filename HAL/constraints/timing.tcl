c496f7def1b5286daa996faab6f20ba8a28aa74a
Andrew Peck <andrew.peck@cern.ch>
2021-10-21 11:01:35 -0400

    hal: add timing exception for axi system reset

--- HAL/constraints/timing.tcl
+++ HAL/constraints/timing.tcl
@@ -9,6 +9,15 @@ foreach refclk [get_clocks refclk*] {
 }
 
 ################################################################################
+# sys_resetter has an asynchronous output (on the axi clock domain) that
+# connects to synchronous reset inputs (on other clock domains) and creates a
+# timing error; just create a false path to prevent timing errors caused by this
+# clock domain crossing
+################################################################################
+
+set_false_path -from [get_pins {top_control_inst/*/sys_reseter/*/*/C}]
+
+################################################################################
 # Uncomment to disable all logic trimming
 ################################################################################
 
