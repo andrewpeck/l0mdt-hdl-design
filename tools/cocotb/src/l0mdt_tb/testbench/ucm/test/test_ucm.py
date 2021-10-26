6ea4c8357ec981d7489db21e185179fbf7dc4048
Priya Sundararajan <priya.sundararajan@cern.ch>
2021-10-26 12:40:22 -0700

    Adding print_tv_bitfields in events.py to print bitfields for tv list

--- tools/cocotb/src/l0mdt_tb/testbench/ucm/test/test_ucm.py
+++ tools/cocotb/src/l0mdt_tb/testbench/ucm/test/test_ucm.py
@@ -268,7 +268,10 @@ def ucm_test(dut):
         output_tv_list.append(single_interface_list)
 
 
+    print("Input testvectors '", input_tv_list)
+    events.print_tv_bitfields(input_tvformats, input_tv_list, UcmPorts.n_input_interfaces, UcmPorts.get_all_input_interface_ports(), num_events_to_process)
 
+ 
     ##
     ## send input events
     ##
