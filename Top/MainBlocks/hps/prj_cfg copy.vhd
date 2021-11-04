9d94759ccf962646dceff3a1f3dadd7df9cf0598
GLdL <guillermo.jose.loustau.de.linares@cern.ch>
2021-11-04 09:23:31 -0400

    fixed gitlab-ci's in MainBlocks projects

--- Top/MainBlocks/hps/prj_cfg copy.vhd
+++ Top/MainBlocks/hps/prj_cfg copy.vhd
@@ -16,7 +16,6 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
- 
 library shared_cfg_def_lib;
 use shared_cfg_def_lib.cfg_global_default_pkg.all;
 
@@ -35,16 +34,16 @@ package body prj_cfg is
   --   --------------------------------------------------------------------------------
   --   -- Sector information
   --   --------------------------------------------------------------------------------
-  --   proj_cfg.SECTOR_ID               := 3,
+    proj_cfg.SECTOR_ID               := 3;
   --   proj_cfg.ST_nBARREL_ENDCAP       := '0', -- 0: barrel    1: Endcap
   --   proj_cfg.ENDCAP_nSMALL_LARGE     := '0', -- 0: small     1: large
-    proj_cfg.ENABLE_NEIGHBORS       := '0'; -- 0: disabled  1: enabled
+  --   proj_cfg.ENABLE_NEIGHBORS        := '1', -- 0: disabled  1: enabled
   --   --------------------------------------------------------------------------------
   --   -- IN COMPILATION CONFIGURATIONS 
   --   --------------------------------------------------------------------------------
   --   proj_cfg.NUM_THREADS             := 3,
   --   proj_cfg.MAX_NUM_HPS             := 3,
-    -- proj_cfg.MAX_NUM_SL              := 5,--3 + to_integer(unsigned'("" & CFG_DEFAULTS.ST_nBARREL_ENDCAP))*to_integer(unsigned'("" & CFG_DEFAULTS.ENDCAP_nSMALL_LARGE))*3 + to_integer(unsigned'("" & CFG_DEFAULTS.ENABLE_NEIGHBORS))*2,
+  --   proj_cfg.MAX_NUM_SL              := 5,--3 + to_integer(unsigned'("" & CFG_DEFAULTS.ST_nBARREL_ENDCAP))*to_integer(unsigned'("" & CFG_DEFAULTS.ENDCAP_nSMALL_LARGE))*3 + to_integer(unsigned'("" & CFG_DEFAULTS.ENABLE_NEIGHBORS))*2,
   --   --------------------------------------------------------------------------------
   --   -- mdt hardware interface config
   --   --------------------------------------------------------------------------------
