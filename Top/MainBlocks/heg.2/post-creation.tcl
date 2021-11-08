3cf35d853b7867f705f66d955490fb5629bba7bc
GLdL <guillermo.jose.loustau.de.linares@cern.ch>
2021-11-08 16:01:13 +0100

    flavour generic

--- Top/MainBlocks/heg.2/post-creation.tcl
+++ Top/MainBlocks/heg.2/post-creation.tcl
@@ -2,3 +2,5 @@ set_property AUTO_INCREMENTAL_CHECKPOINT 1 [get_runs impl_1]
 set_property AUTO_INCREMENTAL_CHECKPOINT 1 [get_runs synth_1]
 set_property -name {riviera.compile.vhdl_relax} -value {true} -objects [get_filesets project_lib_sim]
 set_property -name {riviera.compile.vhdl_syntax} -value {2008} -objects [get_filesets project_lib_sim]
+
+set_property generic FLAVOUR=2 [current_fileset]
