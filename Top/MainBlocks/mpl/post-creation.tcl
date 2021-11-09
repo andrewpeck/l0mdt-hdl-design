cf95b992226a4be3d80ead9d10ac75fe17631dcf
GLdL <guillermo.jose.loustau.de.linares@cern.ch>
2021-11-09 15:21:06 +0100

    post_creataion tcl and new hog.conf for all prjs in MB

--- Top/MainBlocks/mpl/post-creation.tcl
+++ Top/MainBlocks/mpl/post-creation.tcl
@@ -1,4 +1,3 @@
-set_property AUTO_INCREMENTAL_CHECKPOINT 1 [get_runs impl_1]
-set_property AUTO_INCREMENTAL_CHECKPOINT 1 [get_runs synth_1]
+
 set_property -name {riviera.compile.vhdl_relax} -value {true} -objects [get_filesets project_lib_sim]
 set_property -name {riviera.compile.vhdl_syntax} -value {2008} -objects [get_filesets project_lib_sim]
