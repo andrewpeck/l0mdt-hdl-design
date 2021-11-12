2760871af9f427bf902679e2cc825222829e4de1
Davide Cieri <davide.cieri@cern.ch>
2021-11-12 10:30:20 +0100

    FEATURE: ULL Jobs, disabling base_l0mdt

--- Top/ull_vu13p/ull_vu13_b3a_ucm_mpl_hps_lsf_ptc_mpi/prj_cfg.vhd
+++ Top/ull_vu13p/ull_vu13_b3a_ucm_mpl_hps_lsf_ptc_mpi/prj_cfg.vhd
@@ -45,7 +45,7 @@ package body prj_cfg is
     --proj_cfg.INSEL_MDT_nTAR           := '1';
     proj_cfg.ENABLE_UCM               := '1';
     proj_cfg.ENABLE_SF                := '1';
-    proj_cfg.SF_TYPE                  := '0';  -- 0: CSF 1:LSF
+    proj_cfg.SF_TYPE                  := '1';  -- 0: CSF 1:LSF
     proj_cfg.ENABLE_PT                := '1';
     proj_cfg.ENABLE_DAQ               := '0';
     proj_cfg.ENABLE_MTC               := '0';
