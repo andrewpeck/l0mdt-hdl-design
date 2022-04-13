9949615c75faa7e8269010cbde774e06d071b55e
GLdL <guillermo.jose.loustau.de.linares@cern.ch>
2022-04-13 23:44:18 +0200

    moving sim things to shared

--- UserLogic/ucm/top_ucm_sim.vhd
+++ UserLogic/ucm/top_ucm_sim.vhd
@@ -1,15 +1,17 @@
 --------------------------------------------------------------------------------
---  UMass , Physics Department
---  Guillermo Loustau de Linares
---  guillermo.ldl@cern.ch
---------------------------------------------------------------------------------
---  Project: ATLAS L0MDT Trigger 
---  Module: 
---  Description:
---
---------------------------------------------------------------------------------
---  Revisions:
---      
+-- UMass , Physics Department
+-- Project: ATLAS L0MDT Trigger
+-- File: top_ucm_sim.vhd
+-- Module: <<moduleName>>
+-- File PATH: /UserLogic/ucm/top_ucm_sim.vhd
+-- -----
+-- File Created: Monday, 11th April 2022 3:31:05 pm
+-- Author: Guillermo Loustau de Linares (guillermo.ldl@cern.ch)
+-- -----
+-- Last Modified: Tuesday, 12th April 2022 5:06:04 pm
+-- Modified By: Guillermo Loustau de Linares (guillermo.ldl@cern.ch>)
+-- -----
+-- HISTORY:
 --------------------------------------------------------------------------------
 
 library ieee;
@@ -24,6 +26,9 @@ use shared_lib.l0mdt_dataformats_pkg.all;
 use shared_lib.common_constants_pkg.all;
 use shared_lib.common_types_pkg.all;
 use shared_lib.config_pkg.all;
+use shared_lib.detector_param_pkg.all;
+use shared_lib.detector_time_param_pkg.all;
+
 
 library ucm_lib;
 use ucm_lib.ucm_pkg.all;
@@ -70,9 +75,36 @@ end entity ucm_tb;
 
 architecture beh of ucm_tb is
 
-  signal clk                     : std_logic;
-  signal rst                     : std_logic;
-  signal glob_en                 : std_logic;
+  ---------------------------------------------------------------------------
+  -- simulation signals
+  ---------------------------------------------------------------------------
+  -- AXI clk & rst
+  signal axi_rst      : std_logic;
+  signal clk_axi      : std_logic;
+  signal clk_axi_cnt  : integer;
+  -- constant c_CLK_AXI_MULT : integer := 5; 
+  -- clk
+  constant clk_time_period : time := 1 ns;  -- 1Ghz
+  signal clk_time : std_logic := '0';
+  signal tb_curr_time : unsigned(63 downto 0) := (others => '0');
+  -- clk 0.78
+  constant clk_tdc_time_period : time := 0.78125 ns;  
+  signal clk_tdc_time : std_logic := '0';
+  signal tb_curr_tdc_time : unsigned(63 downto 0) := (others => '0');
+  -- clk
+  constant clk_period : time := 3.125 ns;  -- 320Mhz
+  signal clk : std_logic := '0';
+  -- rest
+  constant reset_init_cycles : integer := 3;
+  signal rst: std_logic;
+
+  signal glob_en : std_logic := '1';
+
+  signal bx : std_logic := '0';
+
+  -- signal clk                     : std_logic;
+  -- signal rst                     : std_logic;
+  -- signal glob_en                 : std_logic;
   signal ttc_commands            : l0mdt_ttc_rt;
   -- configuration, control & Monitoring
   signal ctrl_r              : UCM_CTRL_t;
@@ -94,7 +126,7 @@ architecture beh of ucm_tb is
 
 begin
 
-  UCM : entity ucm_lib.ucm
+  UCM_DUT : entity ucm_lib.ucm
   port map(
     clk                     => clk,
     rst                     => rst,
@@ -118,18 +150,125 @@ begin
     o_uCM2pl_av             => o_uCM2pl_av
   );
 
-  -- TH : for th_i in c_NUM_THREADS -1 downto 0 generate
-  --   o_uCM2hps_inn_1b(th_i) <= or_reduce(o_uCM2hps_inn_av(th_i));
-  --   o_uCM2hps_mid_1b(th_i) <= or_reduce(o_uCM2hps_mid_av(th_i));
-  --   o_uCM2hps_out_1b(th_i) <= or_reduce(o_uCM2hps_out_av(th_i));
-  --   o_uCM2hps_ext_1b(th_i) <= or_reduce(o_uCM2hps_ext_av(th_i));
-  -- end generate;
+  -------------------------------------------------------------------------------------
+	-- clock Generator
+	-------------------------------------------------------------------------------------
+  CLK_RT : process begin
+    clk_time <= '0';
+    wait for CLK_time_period/2;
+    clk_time <= '1';
+    wait for CLK_time_period/2;
+  end process;
+  -------------------------------------------------------------------------------------
+	-- clock tdc Generator
+	-------------------------------------------------------------------------------------
+  CLK_TDC : process begin
+    clk_tdc_time <= '0';
+    wait for CLK_tdc_time_period/2;
+    clk_tdc_time <= '1';
+    wait for CLK_tdc_time_period/2;
+  end process;
+  -- clock_and_control.clk <= clk;
+  -------------------------------------------------------------------------------------
+	-- Main FPGA clock
+	-------------------------------------------------------------------------------------
+  CLK_MAIN : process begin
+    clk <= '0';
+    wait for CLK_period/2;
+    clk <= '1';
+    wait for CLK_period/2;
+  end process;
+  clk <= clk;
+  -------------------------------------------------------------------------------------
+  --    AXI CLK
+  -------------------------------------------------------------------------------------
+  axi_clk_proc : process(clk)
+  begin
+    if rising_edge(clk) then
+      if rst = '1' then
+        clk_axi <= '0';
+        clk_axi_cnt <= 0;
+      else
+        if clk_axi_cnt < c_CLK_AXI_MULT then
+          clk_axi_cnt <= clk_axi_cnt + 1;
+        else
+          clk_axi_cnt <= 0;
+          clk_axi <= not clk_axi;
+        end if;
+      end if;
+    end if;
+  end process axi_clk_proc;
+ 	-------------------------------------------------------------------------------------
+	-- Reset Generator
+	-------------------------------------------------------------------------------------
+	rst_process: process begin
+		rst<='0';
+		wait for CLK_period;
+		rst<='1';
+		wait for CLK_period*reset_init_cycles;
+		rst<= '0';
+		wait;
+  end process;
+  rst <= rst;
+  -------------------------------------------------------------------------------------
+	-- Test Bench time
+  -------------------------------------------------------------------------------------
+  ToA: process(clk_time) begin
+    if rising_edge(clk_time) then
+      tb_curr_time <= tb_curr_time + '1';
+    end if;
+  end process;
+  -------------------------------------------------------------------------------------
+	-- Test Bench tdc time
+  -------------------------------------------------------------------------------------
+  ToA_tdc: process(clk_tdc_time) begin
+    if rising_edge(clk_tdc_time) then
+      tb_curr_tdc_time <= tb_curr_tdc_time + '1';
+    end if;
+  end process;
 
-  -- PL : for pl_i in c_MAX_NUM_SL -1 downto 0 generate
-  --   o_uCM2pl_1b(pl_i) <= or_reduce(o_uCM2pl_av(pl_i));
-  -- end generate;
+  -------------------------------------------------------------------------------------
+	-- CSV
+  -------------------------------------------------------------------------------------
+  CSV_SLC_IN : entity shared_lib.csv_reader_slc 
+  generic map (
+    IN_SLC_FILE => IN_SLC_FILE,
+    g_verbose => 2
+  )
+  port map(
+    clk => clk,
+    rst => rst,
+    enable => enable_slc,
+    --
+    tb_curr_tdc_time => tb_curr_tdc_time,
+    -- TAR Hits for simulation
+    o_main_primary_slc    => i_main_primary_slc  ,
+    o_main_secondary_slc  => i_main_secondary_slc,
+    o_plus_neighbor_slc   => i_plus_neighbor_slc ,
+    o_minus_neighbor_slc  => i_minus_neighbor_slc
+    --
+    -- o_slc_event_ai            => slc_event_ai
+  );
 
-
-
+  CSV_UCM_OUT : entity shared_lib.csv_writer_ucm
+  generic map (
+    g_PRJ_INFO    => PRJ_INFO,
+    g_IN_HIT_FILE => IN_HIT_FILE,
+    g_IN_SLC_FILE => IN_SLC_FILE
+  )
+  port map(
+    clk                       => clk,
+    rst                       => rst,
+    enable                    => enable_slc,
+    --
+    tb_curr_tdc_time          => tb_curr_tdc_time
+    --
+    o_uCM2hps_inn_av        => o_uCM2hps_inn_av,
+    o_uCM2hps_mid_av        => o_uCM2hps_mid_av,
+    o_uCM2hps_out_av        => o_uCM2hps_out_av,
+    o_uCM2hps_ext_av        => o_uCM2hps_ext_av,
+    --
+    o_uCM2pl_av             => o_uCM2pl_av
+  );
 
 end beh;
