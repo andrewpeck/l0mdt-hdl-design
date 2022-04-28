df66f3c68fb80e2cd38a49920ece6b612a246435
Andrew Peck <andrew.peck@cern.ch>
2022-04-28 09:45:34 -0400

    fix(c2c): correct post-bitstream scripts for dtsi and svf creation

--- Top/base_l0mdt/post-bitstream.tcl
+++ Top/base_l0mdt/post-bitstream.tcl
@@ -1,10 +1,12 @@
 set SCRIPT_PATH "[file normalize [file dirname [info script]]]"
 set PATH_REPO   "[file normalize ${SCRIPT_PATH}/../../]"
 
+puts "Creating SVF"
 source [file normalize ${SCRIPT_PATH}/../create_svf.tcl]
 
+puts "Copying DTSI Files"
 set DTSI_DIR [file dirname $dst_bit]/dtsi
-set DTSI_FILES [glob $PATH_REPO/kernel/hw/*.dtsi_chunk]
+set DTSI_FILES [glob $PATH_REPO/kernel/hw/*.dtsi]
 file mkdir $DTSI_DIR
 foreach dtsi_file $DTSI_FILES {
     file copy ${dtsi_file}  $DTSI_DIR/
--- Top/create_svf.tcl
+++ Top/create_svf.tcl
@@ -14,13 +14,14 @@ open_hw_target [get_hw_targets -regexp .*/${SVF_TARGET}]
 
 #add the virtex to the chain
 set DEVICE [create_hw_device -part ${FPGA}]
-set_property PROGRAM.FILE $fw_file $DEVICE
+set_property PROGRAM.FILE $dst_bit $DEVICE
 set_param xicom.config_chunk_size 0
 set_property BITSTREAM.GENERAL.COMPRESS TRUE [current_design]
 
-program_hw_devices -force -svf_file [file rootname ${fw_file}].svf ${DEVICE}
+puts "program_hw_devices -force -svf_file [file rootname ${dst_bit}].svf ${DEVICE}"
+program_hw_devices -force -svf_file [file rootname ${dst_bit}].svf ${DEVICE}
 
-write_cfgmem -force -loadbit "up 0 ${fw_file}" \
-    -format mcs -size 128 -file "$[file rootname ${fw_file}].mcs"
+write_cfgmem -force -loadbit "up 0 ${dst_bit}" \
+    -format mcs -size 128 -file "$[file rootname ${dst_bit}].mcs"
 
 delete_hw_target -quiet [get_hw_targets -regexp .*/${SVF_TARGET}]
