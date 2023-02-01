ea117c5dc8622a6fa002cab5086139057e1106e3
GLdL <guillermo.jose.loustau.de.linares@cern.ch>
2023-02-01 13:58:02 +0100

    fixed problem with locked IP

--- UserLogic/ucm/IPs/div_gen_r2s_v1/div_gen_r2s_v1_stub.v
+++ UserLogic/ucm/IPs/div_gen_r2s_v1/div_gen_r2s_v1_stub.v
@@ -1,10 +1,10 @@
 // Copyright 1986-2020 Xilinx, Inc. All Rights Reserved.
 // --------------------------------------------------------------------------------
 // Tool Version: Vivado v.2020.2 (lin64) Build 3064766 Wed Nov 18 09:12:47 MST 2020
-// Date        : Sun Jan 22 18:27:48 2023
+// Date        : Wed Feb  1 13:48:08 2023
 // Host        : VBU2204-1 running 64-bit Ubuntu 22.04.1 LTS
-// Command     : write_verilog -force -mode synth_stub -rename_top div_gen_r2s_v1 -prefix
-//               div_gen_r2s_v1_ div_gen_r2s_v1_stub.v
+// Command     : write_verilog -force -mode synth_stub
+//               /home/guille/ATLAS/l0mdt-hdl-design/UserLogic/ucm/IPs/div_gen_r2s_v1/div_gen_r2s_v1_stub.v
 // Design      : div_gen_r2s_v1
 // Purpose     : Stub declaration of top-level module interface
 // Device      : xcvu13p-flga2577-2-e
