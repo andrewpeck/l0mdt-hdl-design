008583ffa6fc7045f51362cda69f4e6aa0dd8963
GLdL <guillermo.jose.loustau.de.linares@cern.ch>
2023-02-10 15:15:42 +0100

    hog updated

--- Top/ull_vu13p/ull_vu13_ba3_ucm_mpl_hps_csf_bp/prj_cfg.vhd
+++ Top/ull_vu13p/ull_vu13_ba3_ucm_mpl_hps_csf_bp/prj_cfg.vhd
@@ -46,6 +46,7 @@ package body prj_cfg is
     proj_cfg.ENABLE_UCM               := '1';
     proj_cfg.ENABLE_SF                := '1';
     proj_cfg.SF_TYPE                  := '0';  -- 0: CSF 1:LSF
+    proj_cfg.SF_BYPASS                := '1'; 
     proj_cfg.ENABLE_PT                := '0';
     proj_cfg.ENABLE_DAQ               := '0';
     proj_cfg.ENABLE_MTC               := '0';
