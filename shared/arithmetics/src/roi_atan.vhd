e5ffc20a3a1caf4c2ca612a3606d5557df089e41
GLdL <guillermo.jose.loustau.de.linares@cern.ch>
2023-02-16 16:03:17 +0100

    last changes

--- shared/arithmetics/src/roi_atan.vhd
+++ shared/arithmetics/src/roi_atan.vhd
@@ -14,47 +14,45 @@
 -- HISTORY:
 --------------------------------------------------------------------------------
 
-
 library ieee;
-use ieee.std_logic_1164.all;
-use ieee.numeric_std.all;
+  use ieee.std_logic_1164.all;
+  use ieee.numeric_std.all;
 
 library shared_lib;
-use shared_lib.common_ieee_pkg.all;
-use shared_lib.l0mdt_constants_pkg.all;
-use shared_lib.l0mdt_dataformats_pkg.all;
-use shared_lib.common_constants_pkg.all;
-use shared_lib.common_types_pkg.all;
-use shared_lib.config_pkg.all;
-use shared_lib.roi_atan_pkg.all;
-
+  use shared_lib.common_ieee_pkg.all;
+  use shared_lib.l0mdt_constants_pkg.all;
+  use shared_lib.l0mdt_dataformats_pkg.all;
+  use shared_lib.common_constants_pkg.all;
+  use shared_lib.common_types_pkg.all;
+  use shared_lib.config_pkg.all;
+  use shared_lib.roi_atan_pkg.all;
 
 entity roi_atan is
-  generic(
-    g_INPUT_LEN         : integer := 11;
-    g_OUTPUT_LEN        : integer := 11
+  generic (
+    g_input_len  : integer := 11;
+    g_output_len : integer := 11
   );
   port (
-    clk                 : in std_logic;
-    rst                 : in std_logic;
-    ena             : in std_logic;
+    clk : in    std_logic;
+    rst : in    std_logic;
+    ena : in    std_logic;
     --
-    i_slope             : in unsigned(g_INPUT_LEN-1 downto 0);
-    i_dv                : in std_logic;
-    o_mbar              : out unsigned(g_OUTPUT_LEN-1 downto 0);
-    o_dv                : out std_logic
+    i_slope : in    unsigned(g_input_len - 1 downto 0);
+    i_dv    : in    std_logic;
+    o_mbar  : out   unsigned(g_output_len - 1 downto 0);
+    o_dv    : out   std_logic
   );
 end entity roi_atan;
 
 architecture beh of roi_atan is
 
   signal int_data_valid : std_logic;
-  signal addr_mem : unsigned(g_INPUT_LEN-1 downto 0);
+  signal addr_mem       : unsigned(g_input_len - 1 downto 0);
 
   signal mem : roi_atan_lut_t(0 to ROM_ATAN_MAX_SIZE - 1) := ROI_ATAN_MEM;
 
-  attribute ROM_STYLE : string;
-  attribute ROM_STYLE of mem : signal is "distributed";
+  attribute rom_style : string;
+  attribute rom_style of mem : signal is "distributed";
 
 begin
 
@@ -69,36 +67,37 @@ begin
   --     addr_mem <= i_slope;--(DT2R_LARGE_ADDR_LEN -1 downto 0);
   --   -- end if;
   -- end process;
-  
-  MRAD2SLOPE : process(clk)
+
+  mrad2slope : process (clk) is
 
   begin
+
     if rising_edge(clk) then
-      if rst= '1' then
-        o_mbar <= (others => '0');
-        addr_mem <= (others => '0');
+      if (rst= '1') then
+        o_mbar         <= (others => '0');
+        addr_mem       <= (others => '0');
         int_data_valid <= '0';
-        o_dv <= '0';
+        o_dv           <= '0';
       else
-        if ena = '1' then
+        if (ena = '1') then
           int_data_valid <= i_dv;
-          if i_dv = '1' then
+          if (i_dv = '1') then
             addr_mem <= i_slope;
           end if;
 
           o_dv <= int_data_valid;
-          if(int_data_valid = '1') then
-            o_mbar <= to_unsigned(integer(mem(to_integer(addr_mem))),g_OUTPUT_LEN);
+          if (int_data_valid = '1') then
+            o_mbar <= to_unsigned(integer(mem(to_integer(addr_mem))), g_output_len);
           else
             o_mbar <= (others => '0');
           end if;
         else
           o_mbar <= (others => '0');
-          o_dv <= '0';
+          o_dv   <= '0';
         end if;
       end if;
-    end if ;
-  end process;
-  
-  
+    end if;
+
+  end process mrad2slope;
+
 end architecture beh;
