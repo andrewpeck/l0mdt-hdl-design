b55a032eb0aacae146c4509d0a46d3de1ee5abcb
GLdL <guillermo.jose.loustau.de.linares@cern.ch>
2023-02-24 16:25:17 +0100

    addded HAL, HAL_CORE, C2C_INTFS and FW_INFO & more prjs to argos

---
+++ HAL/ctrl/src/FW_INFO/FW_INFO_PKG_DEF.vhd
@@ -0,0 +1,11 @@
+--This file was auto-generated.
+--Modifications might be lost.
+library IEEE;
+use IEEE.std_logic_1164.all;
+library ctrl_lib;
+use ctrl_lib.FW_INFO_CTRL.all;
+
+
+package FW_INFO_CTRL_DEF is
+
+end package FW_INFO_CTRL_DEF;
