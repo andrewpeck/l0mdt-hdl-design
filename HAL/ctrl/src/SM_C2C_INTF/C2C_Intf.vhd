2f4d03f5428fd2d50933418507b421af3ab3e42a
GLdL <guillermo.jose.loustau.de.linares@cern.ch>
2023-02-28 10:49:21 +0100

    IP upgrade again

--- HAL/ctrl/src/SM_C2C_INTF/C2C_Intf.vhd
+++ HAL/ctrl/src/SM_C2C_INTF/C2C_Intf.vhd
@@ -78,9 +78,6 @@ begin
 
   --For AXI
   C2C_INTF_1: entity work.C2C_INTF_map
-    --  generic map (
-    --   --  ALLOCATED_MEMORY_RANGE => ALLOCATED_MEMORY_RANGE
-    --    )
     port map (
       clk_axi         => clk_axi,
       reset_axi_n     => reset_axi_n,
