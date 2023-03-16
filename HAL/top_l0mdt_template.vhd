0d4b09d935d022ef2a74e001ceb0f0974e99de70
Priya Sundararajan <Priya.Sundararajan@cern.ch>
2023-03-15 23:13:10 -0700

    Fixed top_l0mdt_template.vhd - Missing axi_reset_n port got top_control_inst

--- HAL/top_l0mdt_template.vhd
+++ HAL/top_l0mdt_template.vhd
@@ -513,7 +513,7 @@ begin
       c2cb_txp     => c2cb_txp,
       c2c_refclkp => refclk_i_p(C2C_REFCLK_SRC), -- c2c_refclkp, 
       c2c_refclkn => refclk_i_n(C2C_REFCLK_SRC), --c2c_refclkn,
-
+      axi_reset_n => axi_reset_n,
       -- START: ULT_IO :: DO NOT EDIT
       -- END: ULT_IO :: DO NOT EDIT
        
