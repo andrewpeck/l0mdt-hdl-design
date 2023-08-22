40a8871cc25684f0018ac01b999e20bd45274590
Davide Cieri <davide.cieri@cern.ch>
2023-08-22 09:57:51 +0200

    remove old projects

--- Top/l0mdt_ku15p_ull_empty_usp/pre-synthesis.tcl
+++
@@ -1,6 +0,0 @@
-set_msg_config -id {[Synth 8-63]} -limit 10000
-#set_msg_config -id {[Synth 8-331]} -limit 100000
-set_msg_config -id {[Synth 8-3332]} -limit 100000
-#set_msg_config -id {[Synth 8-3331]} -limit 100000
-#set_msg_config -id {[Constraints 18-401]} -limit 100000
-#set_msg_config -id {[Constraints 18-402]} -limit 100000
--- Top/ull_vu13p/ull_vu13_b3a_tar/post-creation.tcl
+++
@@ -1,2 +0,0 @@
-set_property AUTO_INCREMENTAL_CHECKPOINT 1 [get_runs impl_1]
-set_property AUTO_INCREMENTAL_CHECKPOINT 1 [get_runs synth_1]
