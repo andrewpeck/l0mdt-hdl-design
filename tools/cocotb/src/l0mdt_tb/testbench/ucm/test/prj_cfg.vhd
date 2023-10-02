b5ad1b732483e9c1a0c851a53eec891df907c409
Iacopo Longarini <iacopo.longarini@cern.ch>
2023-10-02 02:56:27 -0700

    reflecting changes in UserLogic/ucm/top_ucm_sim.vhd in ucm cocotb testbench

--- tools/cocotb/src/l0mdt_tb/testbench/ucm/test/prj_cfg.vhd
+++ tools/cocotb/src/l0mdt_tb/testbench/ucm/test/prj_cfg.vhd
@@ -1,31 +1,16 @@
---------------------------------------------------------------------------------
---  UMass , Physics Department
---  Guillermo Loustau de Linares
---  gloustau@cern.ch
---
---  Project: ATLAS L0MDT Trigger
---  Module: configuration file
---  Description:
---
---------------------------------------------------------------------------------
---  Revisions:
---    05/02/2020    0.1     File created
---------------------------------------------------------------------------------
-
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
---library shared_lib;
---use shared_lib.cfg_global_default_pkg.all;
 library shared_cfg_def_lib;
 use shared_cfg_def_lib.cfg_global_default_pkg.all;
 
+--library hal;
+--use hal.constants_pkg.all;
+
 package prj_cfg is
 
-
-
- 
+  -- constant CFG : cfg_rt := CFG_DEFAULTS;
   function set_project_cfg return cfg_rt;
 
 end package prj_cfg;
@@ -35,34 +20,81 @@ package body prj_cfg is
   function set_project_cfg return cfg_rt is
     variable proj_cfg : cfg_rt := CFG_DEFAULTS;
   begin
-  --   --------------------------------------------------------------------------------
-  --   -- Sector information
-  --   --------------------------------------------------------------------------------
-    proj_cfg.SECTOR_ID               := 3;
 
-    proj_cfg.ST_nBARREL_ENDCAP       := '0';     -- 0: barrel    1: Endcap
-  --   proj_cfg.ENDCAP_nSMALL_LARGE     := '0', -- 0: small     1: large
-    proj_cfg.ENABLE_NEIGHBORS       := '0'; -- 0: disabled  1: enabled
-  --   --------------------------------------------------------------------------------
-  --   -- IN COMPILATION CONFIGURATIONS
-  --   --------------------------------------------------------------------------------
-    proj_cfg.NUM_THREADS             := 3;
-    proj_cfg.NUM_MTC                 := 3;
+    -- THESE VARIABLES ARE AUTO-SET FROM THE MDT FLAVORS SCRIPT
 
-  --   proj_cfg.MAX_NUM_HPS             := 3,
-    -- proj_cfg.MAX_NUM_SL              := 5,--3 + to_integer(unsigned'("" & CFG_DEFAULTS.ST_nBARREL_ENDCAP))*to_integer(unsigned'("" & CFG_DEFAULTS.ENDCAP_nSMALL_LARGE))*3 + to_integer(unsigned'("" & CFG_DEFAULTS.ENABLE_NEIGHBORS))*2,
-  --   --------------------------------------------------------------------------------
-  --   -- mdt hardware interface config
-  --   --------------------------------------------------------------------------------
-  --   -- numTDCs_lpGBT           := 9,
-  --   -- numlpGBTs_mux           := 2,
-  --   -- numInputs_mux           := numlpGBTs_mux * numTDCs_lpGBT,
-  --   --------------------------------------------------------------------------------
-  --   -- Segment Finder
-  --   --------------------------------------------------------------------------------
-  --   proj_cfg.SF_TYPE                 := '0',  -- 0: CSF 1:LSF
+    -- sector configuration
+    proj_cfg.SECTOR_ID           := 3;
+    proj_cfg.SECTOR_SIDE         := 0;
+    proj_cfg.ST_nBARREL_ENDCAP   := '0';  -- 0: barrel    1: Endcap
+    proj_cfg.ENABLE_NEIGHBORS    := '0';  -- 0: disabled  1: enabled
+    proj_cfg.ENDCAP_nSMALL_LARGE := '0';  -- 0: small     1: large
 
+    -- sf types
+    proj_cfg.SF_TYPE             := '1';  -- 0: CSF       1: LSF
+    proj_cfg.PT_TYPE             := '0';  -- 0: MPT       1: UPT
+
+    -- enables
+    proj_cfg.ENABLE_DAQ          := '0';
+    proj_cfg.ENABLE_SF           := '1';
+    proj_cfg.ENABLE_FM           := '1';
+    proj_cfg.ENABLE_PT           := '0';
+    proj_cfg.ENABLE_UCM          := '1';
+    proj_cfg.ENABLE_H2S          := '1';
+    proj_cfg.ENABLE_MPL          := '0';
+    proj_cfg.ENABLE_MTC          := '0';
+    proj_cfg.ENABLE_TAR          := '0';
+
+    -- END of auto-set variables
+    proj_cfg.NUM_MTC             := 3;    -- default 3, connecting to primary SL
+
+
+    -- take these values from HAL, derived from the link mapping
+    -- proj_cfg.NUM_MDT_CH_INN  := c_NUM_POLMUX_INNER;
+    -- proj_cfg.NUM_MDT_CH_MID  := c_NUM_POLMUX_MIDDLE;
+    -- proj_cfg.NUM_MDT_CH_OUT  := c_NUM_POLMUX_OUTER;
+    -- proj_cfg.NUM_MDT_CH_EXT  := c_NUM_POLMUX_EXTRA;
+
+    --if (c_NUM_POLMUX_INNER > 0) then
+    --  proj_cfg.ENABLE_ST_INN := '1';
+    --  proj_cfg.EN_MDT_CH_INN := (others => '1');
+    --else
+    --  proj_cfg.ENABLE_ST_INN := '0';
+    --  proj_cfg.EN_MDT_CH_INN := (others => '0');
+    --end if;
+
+    --if (c_NUM_POLMUX_MIDDLE > 0) then
+    --  proj_cfg.EN_MDT_CH_MID := (others => '1');
+    --  proj_cfg.ENABLE_ST_MID := '1';
+    --else
+    --  proj_cfg.EN_MDT_CH_MID := (others => '0');
+    --  proj_cfg.ENABLE_ST_MID := '0';
+    --end if;
+
+    --if (c_NUM_POLMUX_OUTER > 0) then
+    --  proj_cfg.EN_MDT_CH_OUT := (others => '1');
+    --  proj_cfg.ENABLE_ST_OUT := '1';
+    --else
+    --  proj_cfg.EN_MDT_CH_OUT := (others => '0');
+    --  proj_cfg.ENABLE_ST_OUT := '0';
+    --end if;
+
+    --if (c_NUM_POLMUX_EXTRA > 0) then
+    --  proj_cfg.EN_MDT_CH_EXT := (others => '1');
+    --  proj_cfg.ENABLE_ST_EXT := '1';
+    --else
+    --  proj_cfg.EN_MDT_CH_EXT := (others => '0');
+    --  proj_cfg.ENABLE_ST_EXT := '0';
+    --end if;
+    proj_cfg.ENABLE_ST_INN  := '1';
+    proj_cfg.EN_MDT_CH_INN := (others => '1');
+    proj_cfg.ENABLE_ST_MID  := '1';
+    proj_cfg.EN_MDT_CH_MID := (others => '1');
+    proj_cfg.ENABLE_ST_OUT := '1';
+    proj_cfg.EN_MDT_CH_OUT := (others => '1');
+    
     return proj_cfg;
+
   end function set_project_cfg;
 
 end package body prj_cfg;
