6e9fcd3dde02da1702b3bc7188c2b0572b03b48f
Iacopo Longarini <iacopo.longarini@cern.ch>
2023-10-10 02:09:21 -0700

    minor changes to setup_env and check_all_tb

--- tools/cocotb/check_all_tb.sh
+++ tools/cocotb/check_all_tb.sh
@@ -11,27 +11,41 @@
 LOGDIR=/home/ilongari/work/l0mdt_test_dev/test_results/$(date -u  +%Y%m%d_%H.%M.%S)/
 mkdir -p $LOGDIR
 echo "Logfiles will be saved in $LOGDIR"
+
+
 ### List of tests
 declare -a cocotb_tests=(
-    ptcalc
-    ptcalc_3threads
-    lsf_inn
-    lsf_mid
-    lsf_out
-    hps_inn
-    mtc
-    ucm
-    ult_tar
-    polmux)
+    polmux                 
+    ult_tar                
+    ucm                        
+    mtc                        
+    mtc_no_sb              
+    mpl_mtc                
+    csf                    
+    lsf_inn                
+    lsf_mid                
+    lsf_out                
+    hps_inn                
+    ptcalc                 
+    ptc_mpi                
+    ptcalc_3threads        
+    ult_mpt                
+    ult_upt                
+    lsf_ptcalc             
+    ult_mpt_mpl_mtc_primary
+    ult_upt_mpl_mtc_primary
+    ult_ucm_pt_mpl_mtc     
+)
+
 
 
 ### Color msg
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m'
-OKSTR=" [${GREEN}   OK${NC}]"
-ERRSTR=" [${RED}ERROR${NC}]"
-
+OKSTR=" [${GREEN}    OK${NC}]"
+PASSSTR=" [${GREEN}PASSED${NC}]"
+ERRSTR=" [${RED} ERROR${NC}]"
 
 ### Do the dirty job
 for test_name in "${cocotb_tests[@]}"
@@ -42,23 +56,20 @@ do
     if ! grep -Eq "INFO +cocotb +Running" ${LOGDIR}/log-${test_name}.txt
     then    
         echo -e "${ERRSTR} cocotb running INFO message not found, check ${LOGDIR}/log-${test_name}.txt"
-        break
     else
         if grep -qE  "TEST.*STATUS.*SIM.*TIME.*(ns)" ${LOGDIR}/log-${test_name}.txt
         then 
             if grep -qE "cocotb.regression.*test passed" ${LOGDIR}/log-${test_name}.txt
             then 
-                echo -e "${OKSTR} Test PASSED" 
+                echo -e "${PASSSTR} Test PASSED - see ${LOGDIR}/log-${test_name}.txt"
             elif grep -q "cocotb.result.TestFailure" ${LOGDIR}/log-${test_name}.txt
             then 
-                echo -e "${OKSTR} Test completed but TV comparison failed" 
+                echo -e "${OKSTR} Test completed but TV comparison failed - see ${LOGDIR}/log-${test_name}.txt" 
             else 
                 echo -e "${ERRSTR} Other failure - check ${LOGDIR}/log-${test_name}.txt" 
-                break
             fi 
         else 
             echo -e "${ERRSTR} Other failure during cocotb test - check ${LOGDIR}/log-${test_name}.txt" 
-            break
         fi
     fi
 done
