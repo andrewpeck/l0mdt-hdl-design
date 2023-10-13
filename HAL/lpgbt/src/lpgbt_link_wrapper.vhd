6f9b72304f103d9e191e71f099e05b24d87743bf
Panagiotis Gkountoumis <panagiotis.gkountoumis@cern.ch>
2023-10-13 15:52:37 +0200

    uploading modified GTY files

--- HAL/lpgbt/src/lpgbt_link_wrapper.vhd
+++ HAL/lpgbt/src/lpgbt_link_wrapper.vhd
@@ -204,16 +204,16 @@ begin
       downlink_mgt_word_array_o(I) <= mgt_data;
     end generate;
     
---    lpgbt_ila_downlink : if (g_debug) generate
---        uplink_lpgbt_ila : ila_downlink 
---        port map(
---            clk => downlink_clk,
---            probe0      => mgt_data,
---            probe1      => downlink_data.data,
---            probe2      => downlink_data.ec,
---            probe3      => downlink_data.ic
---        );    
---    end generate;
+    lpgbt_ila_downlink : if (g_debug) generate
+        uplink_lpgbt_ila : ila_downlink 
+        port map(
+            clk => downlink_clk,
+            probe0      => mgt_data,
+            probe1      => downlink_data.data,
+            probe2      => downlink_data.ec,
+            probe3      => downlink_data.ic
+        );    
+    end generate;
 
   end generate;
 
