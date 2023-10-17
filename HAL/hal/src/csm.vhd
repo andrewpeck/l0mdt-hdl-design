b89971ee77811d3c4220b7f8811e764bbf0a66a6
Davide Cieri <davide.cieri@cern.ch>
2023-10-17 14:54:39 +0200

    missing package import

--- HAL/hal/src/csm.vhd
+++ HAL/hal/src/csm.vhd
@@ -10,6 +10,7 @@ use ctrl_lib.axiRegPkg.all;
 library shared_lib;
 use shared_lib.l0mdt_dataformats_pkg.all;
 use shared_lib.common_types_pkg.all;
+use shared_lib.config_pkg.all;
 
 library hal;
 use hal.all;
